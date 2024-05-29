c3e0caf573ab0711a0b46f9670cac1334cd4aec2
whitedog0980 <whitedog0980@gmail.com>
2024-05-29 12:15:44 +0900

    n18108 Solved

---
+++ src/Input_output_add_etc/C1.java
@@ -0,0 +1,11 @@
+package Input_output_add_etc;
+
+import java.util.Scanner;
+
+public class C1 {
+    public void n18108() {
+        Scanner scanner = new Scanner(System.in);
+        int yearA = scanner.nextInt();
+        System.out.printf(Integer.toString(yearA - 543));
+    }
+}
