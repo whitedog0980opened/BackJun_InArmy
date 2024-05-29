8d076c285688c4759db8b671e219dc07297596ee
whitedog0980 <whitedog0980@gmail.com>
2024-05-29 12:36:08 +0900

    n10430 solved

--- src/Input_output_add_etc/C1.java
+++ src/Input_output_add_etc/C1.java
@@ -8,4 +8,17 @@ public class C1 {
         int yearA = scanner.nextInt();
         System.out.printf(Integer.toString(yearA - 543));
     }
+
+    public void n10430() {
+        Scanner scanner = new Scanner(System.in);
+        String inputs = scanner.nextLine();
+        int[] nums = new int[3];
+        for (int i = 0; i < 3; i++) {
+            nums[i] = Integer.parseInt(inputs.split(" ")[i]);
+        }
+        System.out.println(Integer.toString((nums[0]+nums[1]) % nums[2]));
+        System.out.println(Integer.toString((nums[0]+nums[1]) % nums[2]));
+        System.out.println(Integer.toString((nums[0]*nums[1]) % nums[2]));
+        System.out.println(Integer.toString((nums[0]*nums[1]) % nums[2]));
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import Input_output_add_etc.C1;
 
 public class Main {
     public static void main(String[] args) {
-
+        C1 c = new C1();
+        c.n10430();
     }
 }
