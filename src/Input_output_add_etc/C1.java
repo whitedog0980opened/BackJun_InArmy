629a9799819487bcd19c1c73d9a8fb00ceef4c9b
MINJUN <whitedog0980@gmail.com>
2024-05-31 00:24:11 +0900

    n9499 solved

--- src/Input_output_add_etc/C1.java
+++ src/Input_output_add_etc/C1.java
@@ -79,5 +79,15 @@ public class C1 {
         System.out.printf(result);
     }
 
-
+    public void n9498() {
+        String result;
+        Scanner sc = new Scanner(System.in);
+        int score = Integer.parseInt(sc.nextLine());
+        if (score > 90) {result = "A";}
+        else if (score > 80) {result = "B";}
+        else if (score > 70) {result = "C";}
+        else if (score > 60) {result = "D";}
+        else {result = "F";}
+        System.out.printf(result);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,12 @@ public class Main {
     public static void main(String[] args) {
         String result;
         Scanner sc = new Scanner(System.in);
-        String inputStr = sc.nextLine();
-        String[] inputStrs = inputStr.split(" ");
-        int[] nums = new int[2];
-        nums[0] = Integer.parseInt(inputStrs[0]);
-        nums[1] = Integer.parseInt(inputStrs[1]);
-        if (nums[0] > nums[1]) {result = ">";}
-        else if (nums[0] < nums[1]) {result = "<";}
-        else {result = "==";}
+        int score = Integer.parseInt(sc.nextLine());
+        if (score > 90) {result = "A";}
+        else if (100 >= score && score > 80) {result = "B";}
+        else if (score > 70) {result = "C";}
+        else if (score > 60) {result = "D";}
+        else {result = "F";}
         System.out.printf(result);
     }
 }
