ad30dbca7c90683eebfc06b5a6c2d94c4f7d2904
MINJUN <whitedog0980@gmail.com>
2024-06-10 22:29:43 +0900

    n2562 solved

--- src/Input_output_add_etc/C1.java
+++ src/Input_output_add_etc/C1.java
@@ -222,4 +222,19 @@ public class C1 {
              System.out.println();
          }
      }
+
+     public void n2562() {
+        Scanner sc = new Scanner(System.in);
+        int max = 0;
+        int num = 0;
+        for (int i = 0; i < 9; i++) {
+            int temp = Integer.parseInt(sc.nextLine());
+            if (temp > max) {
+                max = temp;
+                num = i + 1;
+            }
+        }
+        System.out.println(Integer.toString(max));
+        System.out.println(Integer.toString(num));
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,16 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int tries = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i < tries; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.printf("*");
+        int max = 0;
+        int num = 0;
+        for (int i = 0; i < 9; i++) {
+            int temp = Integer.parseInt(sc.nextLine());
+            if (temp > max) {
+                max = temp;
+                num = i + 1;
             }
-            System.out.println();
         }
+        System.out.println(Integer.toString(max));
+        System.out.println(Integer.toString(num));
     }
 }
