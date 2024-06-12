0f57e217e33c2342e101952b13a3d12f12213a7a
MINJUN <whitedog0980@gmail.com>
2024-06-12 21:53:10 +0900

    n1009 solved

--- src/Input_output_add_etc/C1.java
+++ src/Input_output_add_etc/C1.java
@@ -253,4 +253,20 @@ public class C1 {
             System.out.println();
         }
     }
+
+    public void n1009() {
+        Scanner sc = new Scanner(System.in);
+        int tries = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < tries; i++) {
+            String[] inputs = sc.nextLine().split(" ");
+            int a = Integer.parseInt(inputs[0]);
+            a = a % 10;
+            int b = Integer.parseInt(inputs[1]);
+            b = b % 4;
+            if (b == 0) {b = 4;}
+            int result = (int)Math.pow(a, b) % 10;
+            if (result == 0) {result = 10;}
+            System.out.println(Integer.toString(result));
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,18 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int tries = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < tries; i++) {
+            String[] inputs = sc.nextLine().split(" ");
+            int a = Integer.parseInt(inputs[0]);
+            a = a % 10;
+            int b = Integer.parseInt(inputs[1]);
+            b = b % 4;
+            if (b == 0) {b = 4;}
+            int result = (int)Math.pow(a, b) % 10;
+            if (result == 0) {result = 10;}
+            System.out.println(Integer.toString(result));
+        }
     }
 }
