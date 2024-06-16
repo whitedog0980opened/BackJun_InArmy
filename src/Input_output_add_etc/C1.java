76dd4561c09b6d19dbf294487dfc2f04a56e206a
MINJUN <whitedog0980@gmail.com>
2024-06-16 22:35:52 +0900

    n10810 solved

--- src/Input_output_add_etc/C1.java
+++ src/Input_output_add_etc/C1.java
@@ -269,4 +269,28 @@ public class C1 {
             System.out.println(Integer.toString(result));
         }
     }
+
+    public void n10818() {
+        Scanner sc = new Scanner(System.in);
+        int tries = Integer.parseInt(sc.nextLine());
+        String[] numsStr = sc.nextLine().split(" ");
+
+        int[] nums = new int[tries];
+        for (int i = 0; i < tries; i++) {
+            nums[i] = Integer.parseInt(numsStr[i]);
+        }
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i: nums) {
+            if (i > max) {
+                max = i;
+            }
+            if (i < min) {
+                min = i;
+            }
+        }
+
+        System.out.printf("%d %d", min, max);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,64 +2,26 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Receive Test Case Num
         Scanner sc = new Scanner(System.in);
-        int testCase = Integer.parseInt(sc.nextLine());
+        int tries = Integer.parseInt(sc.nextLine());
+        String[] numsStr = sc.nextLine().split(" ");
 
-        for (int i = 0; i < testCase; i++) {
-            String[] nums = sc.nextLine().split(" ");
-            buildTimeCalculator(Integer.parseInt(nums[0]), Integer.parseInt(nums[1]), sc);
-        }
-    }
-
-    public static void buildTimeCalculator(int buildsNum, int sequencesNum, Scanner sc) {
-        // Receive required times for build
-        String[] requireTimeStrs = sc.nextLine().split(" ");
-        int[] requireTimes = new int[buildsNum + 1]; // +1 Because Number of Building is start counting from 1
-        for (int i = 0; i < buildsNum; i++) {
-            requireTimes[i + 1] = Integer.parseInt(requireTimeStrs[i]);
+        int[] nums = new int[tries];
+        for (int i = 0; i < tries; i++) {
+            nums[i] = Integer.parseInt(numsStr[i]);
         }
 
-        // Receive sequence of buildings
-        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
-        for (int i = 0; i <= buildsNum; i++) {
-            adjList.add(new ArrayList<>());
-        }
-        for (int i = 0; i < sequencesNum; i++) {
-            String[] temp = sc.nextLine().split(" ");
-            int X = Integer.parseInt(temp[0]);
-            int Y = Integer.parseInt(temp[1]);
-            adjList.get(Y).add(X); // Storing in reverse order for backtracking
-        }
-
-        // Receive target building
-        int targetBuild = Integer.parseInt(sc.nextLine());
-
-        // Find the maximum time to build the target building
-        boolean[] visited = new boolean[buildsNum + 1];
-        int[] dp = new int[buildsNum + 1];
-        Arrays.fill(dp, -1);
-        int result = findMaxTime(targetBuild, adjList, requireTimes, dp, visited);
-        System.out.println(result);
-    }
-
-    public static int findMaxTime(int current, ArrayList<ArrayList<Integer>> adjList, int[] requireTimes, int[] dp, boolean[] visited) {
-        if (dp[current] != -1) {
-            return dp[current];
-        }
-        if (adjList.get(current).isEmpty()) {
-            dp[current] = requireTimes[current];
-            return dp[current];
-        }
-        int maxTime = 0;
-        for (int prev : adjList.get(current)) {
-            if (!visited[prev]) {
-                visited[prev] = true;
-                maxTime = Math.max(maxTime, findMaxTime(prev, adjList, requireTimes, dp, visited));
-                visited[prev] = false;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i: nums) {
+            if (i > max) {
+                max = i;
+            }
+            if (i < min) {
+                min = i;
             }
         }
-        dp[current] = maxTime + requireTimes[current];
-        return dp[current];
+
+        System.out.printf("%d %d", min, max);
     }
 }
