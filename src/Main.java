653ac6c898f71fefd78425df530a4708edb54800
whitedog0980opened <whitedog0980opened@gmail.com>
2025-10-18 14:49:20 +0000

    n1018 soveling(not yet)

--- src/Main.java
+++ src/Main.java
@@ -7,26 +7,54 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int StudentNum = Integer.parseInt(br.readLine());
-        List<Integer> retester = new ArrayList<>();
+        // init first input
+        String[] boardSizeStr = br.readLine().split(" ");
+        int[] boardSize = new int[2];
+        boardSize[0] = Integer.parseInt(boardSizeStr[0]); // num of row
+        boardSize[1] = Integer.parseInt(boardSizeStr[1]); // num of column
 
-        A : for (int i = 0; i < StudentNum; i++) {
-            String[] strScores = br.readLine().split(" ");
-            for (int j = 0; j < 10; j++) {
-                int correctAnswer = (j) % 5 + 1;
-                int score = Integer.parseInt(strScores[j]);
-                if (score != correctAnswer) {
-                    continue A;
-                }
+        // init board data
+        // 'W' = true , 'B' = false
+        boolean[][] board = new boolean[boardSize[0]][boardSize[1]];
+        for (int i = 0; i < boardSize[0]; i++) {
+            char[] rowCell = br.readLine().toCharArray();
+            for (int j = 0; j < boardSize[1]; j++) {
+                boolean cell = (rowCell[j] == 'W' ? true : false);
+                board[i][j] = cell;
             }
-            retester.add(i + 1);
-            
         }
 
-        for (int i : retester) {
-            bw.write(i + "\n");
-        }
+        //
+
+
+
+
         bw.flush();
         bw.close();
     }
+
+    // culculate score of cutten board
+    // firstColor : color of first cell of cutten board
+    public static int culculateScore(boolean[][] cuttenBoard, boolean firstColor) {
+        int score = 0;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                // get current cell color
+                boolean currentColor;
+                if (i + j % 2 == 0) { currentColor = firstColor; }
+                else { currentColor = !firstColor;}
+
+                if (cuttenBoard[i][j] == currentColor) {
+                    score++;
+                }
+            }
+        }
+
+        if (score > 32) {
+            return score;
+        } else { // when board is entirely opposite color
+            return 64 - score;
+        }
+    }
+
 }
