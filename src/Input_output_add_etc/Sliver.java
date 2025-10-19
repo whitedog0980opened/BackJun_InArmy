4082fc66e993cf551ca410708cbe83dcb23955d7
whitedog0980opened <whitedog0980opened@gmail.com>
2025-10-19 13:38:10 +0000

    n10814 solved

--- src/Input_output_add_etc/Sliver.java
+++ src/Input_output_add_etc/Sliver.java
@@ -1,8 +1,4 @@
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.*;
 import java.io.*;
 
@@ -285,6 +281,53 @@ public class Sliver {
             bw.write(Integer.toString(i));
             bw.newLine();
         }
+        bw.close();
+    }
+
+    public static void n10814() throws IOException {
+        //member class
+        class Member {
+            int age;
+            String name;
+            Member(int age, String name) {
+                this.age = age;
+                this.name = name;
+            }
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // get first line input = member num
+        int memberNum = Integer.parseInt(br.readLine());
+
+        //unsorted member list
+        List<Member> memberList = new ArrayList<>();
+        for (int i = 0; i < memberNum; i++) {
+            //get memmber info
+            String[] memberInput = br.readLine().split(" ");
+            int age = Integer.parseInt(memberInput[0]);
+            String name = memberInput[1];
+
+            //create member object
+            Member member = new Member(age, name);
+            memberList.add(member);
+        }
+
+        //sort member list by age
+        Collections.sort(memberList, (m1, m2) -> {
+            if (m1.age == m2.age) return 0;
+            else if (m1.age > m2.age) return 1;
+            else return -1;
+        });
+
+        //output
+        for (int i = 0; i < memberNum; i++) {
+            Member member = memberList.get(i);
+            bw.write(member.age + " " + member.name + "\n");
+        }
+
+        bw.flush();
         bw.close();
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,53 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        // init first input
-        String[] boardSizeStr = br.readLine().split(" ");
-        int[] boardSize = new int[2];
-        boardSize[0] = Integer.parseInt(boardSizeStr[0]); // num of row
-        boardSize[1] = Integer.parseInt(boardSizeStr[1]); // num of column
-
-        // init board data
-        // 'W' = true , 'B' = false
-        boolean[][] board = new boolean[boardSize[0]][boardSize[1]];
-        for (int i = 0; i < boardSize[0]; i++) {
-            char[] rowCell = br.readLine().toCharArray();
-            for (int j = 0; j < boardSize[1]; j++) {
-                boolean cell = (rowCell[j] == 'W' ? true : false);
-                board[i][j] = cell;
+        //member class
+        class Member {
+            int age;
+            String name;
+            Member(int age, String name) {
+                this.age = age;
+                this.name = name;
             }
         }
 
-        //
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        // get first line input = member num
+        int memberNum = Integer.parseInt(br.readLine());
 
+        //unsorted member list
+        List<Member> memberList = new ArrayList<>();
+        for (int i = 0; i < memberNum; i++) {
+            //get memmber info
+            String[] memberInput = br.readLine().split(" ");
+            int age = Integer.parseInt(memberInput[0]);
+            String name = memberInput[1];
 
+            //create member object
+            Member member = new Member(age, name);
+            memberList.add(member);
+        }
+
+        //sort member list by age
+        Collections.sort(memberList, (m1, m2) -> {
+            if (m1.age == m2.age) return 0;
+            else if (m1.age > m2.age) return 1;
+            else return -1;
+        });
+
+        //output
+        for (int i = 0; i < memberNum; i++) {
+            Member member = memberList.get(i);
+            bw.write(member.age + " " + member.name + "\n");
+        }
 
         bw.flush();
         bw.close();
     }
 
-    // culculate score of cutten board
-    // firstColor : color of first cell of cutten board
-    public static int culculateScore(boolean[][] cuttenBoard, boolean firstColor) {
-        int score = 0;
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                // get current cell color
-                boolean currentColor;
-                if (i + j % 2 == 0) { currentColor = firstColor; }
-                else { currentColor = !firstColor;}
-
-                if (cuttenBoard[i][j] == currentColor) {
-                    score++;
-                }
-            }
-        }
-
-        if (score > 32) {
-            return score;
-        } else { // when board is entirely opposite color
-            return 64 - score;
-        }
-    }
 
 }
