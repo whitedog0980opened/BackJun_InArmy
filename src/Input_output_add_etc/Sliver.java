a6f0ea0ce96c44c46409b05142e613b9094db0a5
whitedog0980opened <whitedog0980opened@gmail.com>
2025-10-21 07:43:48 +0000

    n11866 solved

--- src/Input_output_add_etc/Sliver.java
+++ src/Input_output_add_etc/Sliver.java
@@ -501,6 +501,51 @@ public class Sliver {
         bw.flush();
         bw.close();
     }
+
+    public static void n11866() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] inputs = br.readLine().split(" ");
+        int n = Integer.parseInt(inputs[0]);
+        int k = Integer.parseInt(inputs[1]);
+
+        //순열 생성
+        List<Integer> permutation = new LinkedList<>();
+
+        List<Integer> answerPmt = new LinkedList<>(); //요세푸스 순열
+        for (int i = 0; i < n; i++) {
+            permutation.add(i + 1);
+        }
+
+        //요세푸스 문제에 맞추어 출력
+        Iterator<Integer> pmtIterater = permutation.iterator();
+        while (true) {
+            if (permutation.isEmpty()) break;
+            for (int i = 0; i < k; i++) {
+                if (!pmtIterater.hasNext()) { //반복이 끝났을 겨우
+                    pmtIterater = permutation.iterator(); //새로운 반복자 적용
+                }
+                if (i + 1 == k) {
+                    answerPmt.add(pmtIterater.next());
+                    pmtIterater.remove();
+                    continue;
+                }
+                pmtIterater.next();
+            }
+        }
+
+        // 출력 인덱스
+        bw.write("<");
+        for (int i = 0; i < n - 1; i++) {
+            bw.write(String.valueOf(answerPmt.get(i)) + ", ");
+        }
+        bw.write(String.valueOf(answerPmt.get(n - 1)) + ">");
+
+
+        bw.flush();
+        bw.close();
+    }
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -3,42 +3,45 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        class Factor {
-            public int x;
-            public int y;
-            Factor (int x, int y) {
-                this.x = x;
-                this.y = y;
-            }
-        }
-
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int inputNum = Integer.parseInt(br.readLine());
+        String[] inputs = br.readLine().split(" ");
+        int n = Integer.parseInt(inputs[0]);
+        int k = Integer.parseInt(inputs[1]);
 
-        List<Factor> factors = new ArrayList<>();
-        for (int i = 0; i < inputNum; i++) {
-            String[] inputs = br.readLine().split(" ");
-            Factor factor = new Factor(Integer.parseInt(inputs[0]), Integer.parseInt(inputs[1]));
-            factors.add(factor);
+        //순열 생성
+        List<Integer> permutation = new LinkedList<>();
+
+        List<Integer> answerPmt = new LinkedList<>(); //요세푸스 순열
+        for (int i = 0; i < n; i++) {
+            permutation.add(i + 1);
         }
 
-        Collections.sort(factors, (x, y) -> {
-            if (x.x < y.x) return -1;
-            else if (x.x == y.x) {
-                if (x.y < y.y) return -1;
-                else if (x.y == y.y) return 0;
-                else return 1;
-            } else {
-                return 1;
+        //요세푸스 문제에 맞추어 출력
+        Iterator<Integer> pmtIterater = permutation.iterator();
+        while (true) {
+            if (permutation.isEmpty()) break;
+            for (int i = 0; i < k; i++) {
+                if (!pmtIterater.hasNext()) { //반복이 끝났을 겨우
+                    pmtIterater = permutation.iterator(); //새로운 반복자 적용
+                }
+                if (i + 1 == k) {
+                    answerPmt.add(pmtIterater.next());
+                    pmtIterater.remove();
+                    continue;
+                }
+                pmtIterater.next();
             }
-        });
-
-        for (Factor factor : factors) {
-            bw.write(factor.x + " " + factor.y + "\n");
         }
+
+        // 출력 인덱스
+        bw.write("<");
+        for (int i = 0; i < n - 1; i++) {
+            bw.write(String.valueOf(answerPmt.get(i)) + ", ");
+        }
+        bw.write(String.valueOf(answerPmt.get(n - 1)) + ">");
+
 
         bw.flush();
         bw.close();
