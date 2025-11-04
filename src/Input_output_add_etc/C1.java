7c6d13e71c7c0d35ba0d550b6f599f88597946a6
whitedog0980opened <whitedog0980opened@gmail.com>
2025-11-04 14:38:38 +0000

    n2775 solved

--- src/Input_output_add_etc/C1.java
+++ src/Input_output_add_etc/C1.java
@@ -570,4 +570,37 @@ public class C1 {
         bw.close();
     }
 
+    public void n2775() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int testCaseNum = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < testCaseNum; i++) {
+            int floor = Integer.parseInt(br.readLine());
+            int roomNum = Integer.parseInt(br.readLine());
+            int[][] peopleNumDp = new int[floor + 1][roomNum + 1];
+            //init 0 floor peopleNum
+            for (int j = 1; j <= roomNum; j++) {
+                peopleNumDp[0][j] = j;
+            }
+            //init roomNum 1 = 1
+            for (int j = 1; j <= floor; j++) {
+                peopleNumDp[j][1] = 1;
+            }
+
+            for (int j = 1; j <= floor; j++) {
+                for (int k = 2; k <= roomNum; k++) {
+                    peopleNumDp[j][k] = peopleNumDp[j - 1][k] + peopleNumDp[j][k - 1];
+                }
+            }
+            bw.write(Integer.toString(peopleNumDp[floor][roomNum]));
+            if (i + 1 != testCaseNum) bw.newLine();
+        }
+
+
+        bw.flush();
+        bw.close();
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -9,71 +9,30 @@ public class Main {
         int testCaseNum = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < testCaseNum; i++) {
-            String[] strInputs = br.readLine().split(" ");
-            int n = Integer.parseInt(strInputs[0]);
-            int m = Integer.parseInt(strInputs[1]);
-            int numOfCabbage = Integer.parseInt(strInputs[2]);
-            int cattapliler = 0;
-            boolean[][] cabbageMap = new boolean[m + 1][n + 1];
-            //init cabbageMap
-            for (int j = 0; j < numOfCabbage; j++) {
-                String[] cabbageLocation = br.readLine().split(" ");
-                int clM = Integer.parseInt(cabbageLocation[1]);
-                int clN = Integer.parseInt(cabbageLocation[0]);
-
-                cabbageMap[clM][clN] = true;
+            int floor = Integer.parseInt(br.readLine());
+            int roomNum = Integer.parseInt(br.readLine());
+            int[][] peopleNumDp = new int[floor + 1][roomNum + 1];
+            //init 0 floor peopleNum
+            for (int j = 1; j <= roomNum; j++) {
+                peopleNumDp[0][j] = j;
+            }
+            //init roomNum 1 = 1
+            for (int j = 1; j <= floor; j++) {
+                peopleNumDp[j][1] = 1;
             }
 
-            //counting catta
-            for (int k = 0; k < n; k++) {
-                if (cabbageMap[0][k] == true) {
-                    cattapliler++;
-                    deleteAdjacentFirstLine(0, k, cabbageMap);
+            for (int j = 1; j <= floor; j++) {
+                for (int k = 2; k <= roomNum; k++) {
+                    peopleNumDp[j][k] = peopleNumDp[j - 1][k] + peopleNumDp[j][k - 1];
                 }
             }
-            for (int j = 1; j < m; j++) { //otherLines
-                for (int k = 0; k < n; k++) {
-                    if (cabbageMap[j][k] == true) {
-                        cattapliler++;
-                        deleteAdjacent(j, k, cabbageMap);
-                    }
-                }
-            }
-            bw.write(Integer.toString(cattapliler));
+            bw.write(Integer.toString(peopleNumDp[floor][roomNum]));
             if (i + 1 != testCaseNum) bw.newLine();
         }
 
 
         bw.flush();
         bw.close();
-    }
-
-    public static void deleteAdjacent(int m,int n, boolean[][] cabbageMap) {
-        cabbageMap[m][n] = false;
-        if (cabbageMap[m + 1][n] == true) {
-            deleteAdjacent(m + 1, n, cabbageMap);
-        }
-        if (cabbageMap[m][n + 1] == true) {
-            deleteAdjacent(m, n + 1, cabbageMap);
-        }
-        if (cabbageMap[m - 1][n] == true) {
-            if (m - 1 == 0) {
-                deleteAdjacentFirstLine(0, n, cabbageMap);
-            } else {
-                deleteAdjacent(m - 1, n, cabbageMap);
-            }
-        }
-    }
-
-
-    public static void deleteAdjacentFirstLine(int m,int n, boolean[][] cabbageMap) {
-        cabbageMap[m][n] = false;
-        if (cabbageMap[m + 1][n] == true) {
-            deleteAdjacent(m + 1, n, cabbageMap);
-        }
-        if (cabbageMap[m][n + 1] == true) {
-            deleteAdjacentFirstLine(m, n + 1, cabbageMap);
-        }
     }
 
 }
