dcab9b8dc3c4f8f54700c749886969baea40c7d6
whitedog0980opened <whitedog0980opened@gmail.com>
2025-11-05 14:55:16 +0000

    n1260 solved

--- src/Input_output_add_etc/Sliver.java
+++ src/Input_output_add_etc/Sliver.java
@@ -622,6 +622,85 @@ public class Sliver {
             }
         }
     }
+
+    static void n1260() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] strInputs = br.readLine().split(" ");
+        int V = Integer.parseInt(strInputs[0]); //정점의 갯수
+        int lineNum = Integer.parseInt(strInputs[1]);
+        int startPoint = Integer.parseInt(strInputs[2]);
+
+
+        //init graph line list
+        LinkedList<Integer>[] graph = new LinkedList[V + 1];
+        for (int i = 1; i <= V; i++) {
+            graph[i] = new LinkedList<>();
+        }
+
+        //get graph lines
+        for (int i = 0; i < lineNum; i++) {
+            String[] edgeInputs = br.readLine().split(" ");
+            int from = Integer.parseInt(edgeInputs[0]);
+            int to = Integer.parseInt(edgeInputs[1]);
+            graph[from].add(to);
+            graph[to].add(from);
+        }
+
+        //sort graph lines
+        for (int i = 1; i <= V; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        //DFS
+        boolean[] visited = new boolean[V + 1];
+        LinkedList<Integer> bfsList = new LinkedList<>();
+        bfsList.push(startPoint);
+        visited[startPoint] = true;
+
+        bw.write(Integer.toString(startPoint));
+        dfs1260(graph, visited, startPoint, bw);
+        bw.newLine();
+        visited = new boolean[V + 1];
+        visited[startPoint] = true;
+        bw.write(Integer.toString(startPoint));
+
+        while (!bfsList.isEmpty()) {
+            int current = bfsList.pop();
+            
+            if (!graph[current].isEmpty()) {
+                Iterator<Integer> i = graph[current].listIterator();
+                while (i.hasNext()) {
+                    int temp = i.next();
+                    if (!visited[temp]) {
+                        visited[temp] = true;
+                        bfsList.add(temp);
+                        bw.write(" " + Integer.toString(temp));
+                    }
+                }
+            }
+        }
+
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static void dfs1260(LinkedList<Integer>[] graph, boolean[] visited, int current, BufferedWriter bw) throws IOException{
+        if (!graph[current].isEmpty()) {
+            for (int i = 0; i < graph[current].size(); i++) {
+                int nextCurrent = graph[current].get(i);
+                if (!visited[nextCurrent]) {
+                    visited[nextCurrent] = true;
+                    bw.write(" " + Integer.toString(nextCurrent));
+                    dfs1260(graph, visited, nextCurrent, bw);
+                }
+            }
+        }
+    }
+
+    
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -6,33 +6,77 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int testCaseNum = Integer.parseInt(br.readLine());
+        String[] strInputs = br.readLine().split(" ");
+        int V = Integer.parseInt(strInputs[0]); //정점의 갯수
+        int lineNum = Integer.parseInt(strInputs[1]);
+        int startPoint = Integer.parseInt(strInputs[2]);
 
-        for (int i = 0; i < testCaseNum; i++) {
-            int floor = Integer.parseInt(br.readLine());
-            int roomNum = Integer.parseInt(br.readLine());
-            int[][] peopleNumDp = new int[floor + 1][roomNum + 1];
-            //init 0 floor peopleNum
-            for (int j = 1; j <= roomNum; j++) {
-                peopleNumDp[0][j] = j;
-            }
-            //init roomNum 1 = 1
-            for (int j = 1; j <= floor; j++) {
-                peopleNumDp[j][1] = 1;
-            }
 
-            for (int j = 1; j <= floor; j++) {
-                for (int k = 2; k <= roomNum; k++) {
-                    peopleNumDp[j][k] = peopleNumDp[j - 1][k] + peopleNumDp[j][k - 1];
+        //init graph line list
+        LinkedList<Integer>[] graph = new LinkedList[V + 1];
+        for (int i = 1; i <= V; i++) {
+            graph[i] = new LinkedList<>();
+        }
+
+        //get graph lines
+        for (int i = 0; i < lineNum; i++) {
+            String[] edgeInputs = br.readLine().split(" ");
+            int from = Integer.parseInt(edgeInputs[0]);
+            int to = Integer.parseInt(edgeInputs[1]);
+            graph[from].add(to);
+            graph[to].add(from);
+        }
+
+        //sort graph lines
+        for (int i = 1; i <= V; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        //DFS
+        boolean[] visited = new boolean[V + 1];
+        LinkedList<Integer> bfsStack = new LinkedList<>();
+        bfsStack.push(startPoint);
+        visited[startPoint] = true;
+
+        bw.write(Integer.toString(startPoint));
+        dfs(graph, visited, startPoint, bw);
+        bw.newLine();
+        visited = new boolean[V + 1];
+        visited[startPoint] = true;
+        bw.write(Integer.toString(startPoint));
+
+        while (!bfsStack.isEmpty()) {
+            int current = bfsStack.pop();
+            
+            if (!graph[current].isEmpty()) {
+                Iterator<Integer> i = graph[current].listIterator();
+                while (i.hasNext()) {
+                    int temp = i.next();
+                    if (!visited[temp]) {
+                        visited[temp] = true;
+                        bfsStack.add(temp);
+                        bw.write(" " + Integer.toString(temp));
+                    }
                 }
             }
-            bw.write(Integer.toString(peopleNumDp[floor][roomNum]));
-            if (i + 1 != testCaseNum) bw.newLine();
         }
 
 
         bw.flush();
         bw.close();
+    }
+
+    public static void dfs(LinkedList<Integer>[] graph, boolean[] visited, int current, BufferedWriter bw) throws IOException{
+        if (!graph[current].isEmpty()) {
+            for (int i = 0; i < graph[current].size(); i++) {
+                int nextCurrent = graph[current].get(i);
+                if (!visited[nextCurrent]) {
+                    visited[nextCurrent] = true;
+                    bw.write(" " + Integer.toString(nextCurrent));
+                    dfs(graph, visited, nextCurrent, bw);
+                }
+            }
+        }
     }
 
 }
