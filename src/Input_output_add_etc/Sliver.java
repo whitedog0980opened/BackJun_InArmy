4673dc544f96bd42a8c2e47ece404932aac5fecf
whitedog0980opened <whitedog0980opened@gmail.com>
2025-11-11 14:29:01 +0000

    n7568 solved

--- src/Input_output_add_etc/Sliver.java
+++ src/Input_output_add_etc/Sliver.java
@@ -757,6 +757,68 @@ public class Sliver {
         bw.close();
     }
 
+    static void n7568() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int peopleNum = Integer.parseInt(br.readLine());
+
+        int[][] bodyData = new int[peopleNum][2];
+        for (int i = 0; i < peopleNum; i++) {
+            String[] inputStrs = br.readLine().split(" ");
+            int weight = Integer.parseInt(inputStrs[0]);
+            int height = Integer.parseInt(inputStrs[1]);
+
+            bodyData[i][0] = weight;
+            bodyData[i][1] = height;
+        }
+
+        int point[] = new int[peopleNum];
+        for (int i = 0; i < peopleNum; i++) {
+            for (int j = 0; j < peopleNum; j++) {
+                point[i] = (bodyData[i][0] < bodyData[j][0] 
+                            && bodyData[i][1] < bodyData[j][1]
+                            ) ? ++point[i] : point[i];
+            }
+        }
+
+        // //compare score
+        // int[] grade = new int[peopleNum];
+        // int crrGrade = 0;
+        // int sameStack = 0;
+        // int max = Integer.MAX_VALUE;
+        // for (int i = 0; i < peopleNum; i++) {
+        //     int maxI = 0;
+        //     int maxCrr = -1;
+        //     for (int j = 0; j < peopleNum; j++) {
+        //         if (maxCrr < point[j]) {
+        //             maxCrr = point[j];
+        //             maxI = j;
+        //         }
+        //     }
+        //     if (max > maxCrr) { //if max value down -> grade++
+        //         crrGrade += sameStack + 1;
+        //         max = maxCrr;
+        //         sameStack = 0;
+        //     } else {
+        //         sameStack++;
+        //     }
+        //     point[maxI] = -1;
+        //     grade[maxI] = crrGrade;
+        // }
+
+        boolean isFirst = true;
+        for (int i : point) {
+            if (isFirst) isFirst = false;
+            else bw.write(" ");
+            bw.write(Integer.toString(i + 1));
+        }
+        
+
+        bw.flush();
+        bw.close();
+    }
+
     
 }
 
--- src/Main.java
+++ src/Main.java
@@ -6,52 +6,57 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        
-        Stack<Integer> stack = new Stack<>();
-        boolean checker = true;
-        boolean isFirst = true;
-        while (true) {
-            String str = br.readLine();
-            if (str.equals(".")) break;
-            checker = true;
-            stack.clear();
-            for (int i = 0; i < str.length(); i++) {
-                char ch = str.charAt(i);
-                if (ch == '(') stack.add(1); // 1 means "("
-                else if (ch == ')') {
-                    if (stack.isEmpty()) {
-                        checker = false;
-                        break;
-                    }
-                    if (stack.pop() != 1) {
-                        checker = false;
-                        break;
-                    }
-                }
-                else if (ch == '[') stack.add(2); // 2 means "["
-                else if (ch == ']') {
-                    if (stack.isEmpty()) {
-                        checker = false;
-                        break;
-                    }
-                    if (stack.pop() != 2) {
-                        checker = false;
-                        break;
-                    }
-                }
-            }
-            if (!stack.empty()) {
-                checker = false;
-                stack.clear();
-            }
+        int peopleNum = Integer.parseInt(br.readLine());
 
-            if (isFirst) {
-                isFirst = false;
-            } else {
-                bw.newLine();
+        int[][] bodyData = new int[peopleNum][2];
+        for (int i = 0; i < peopleNum; i++) {
+            String[] inputStrs = br.readLine().split(" ");
+            int weight = Integer.parseInt(inputStrs[0]);
+            int height = Integer.parseInt(inputStrs[1]);
+
+            bodyData[i][0] = weight;
+            bodyData[i][1] = height;
+        }
+
+        int point[] = new int[peopleNum];
+        for (int i = 0; i < peopleNum; i++) {
+            for (int j = 0; j < peopleNum; j++) {
+                point[i] = (bodyData[i][0] < bodyData[j][0] 
+                            && bodyData[i][1] < bodyData[j][1]
+                            ) ? ++point[i] : point[i];
             }
-            bw.write(checker ? "yes" : "no");
-            
+        }
+
+        // //compare score
+        // int[] grade = new int[peopleNum];
+        // int crrGrade = 0;
+        // int sameStack = 0;
+        // int max = Integer.MAX_VALUE;
+        // for (int i = 0; i < peopleNum; i++) {
+        //     int maxI = 0;
+        //     int maxCrr = -1;
+        //     for (int j = 0; j < peopleNum; j++) {
+        //         if (maxCrr < point[j]) {
+        //             maxCrr = point[j];
+        //             maxI = j;
+        //         }
+        //     }
+        //     if (max > maxCrr) { //if max value down -> grade++
+        //         crrGrade += sameStack + 1;
+        //         max = maxCrr;
+        //         sameStack = 0;
+        //     } else {
+        //         sameStack++;
+        //     }
+        //     point[maxI] = -1;
+        //     grade[maxI] = crrGrade;
+        // }
+
+        boolean isFirst = true;
+        for (int i : point) {
+            if (isFirst) isFirst = false;
+            else bw.write(" ");
+            bw.write(Integer.toString(i + 1));
         }
         
 
