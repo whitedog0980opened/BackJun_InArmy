e089ab4ef4ac675607af59937304c2283963001a
whitedog0980opened <whitedog0980opened@gmail.com>
2025-11-14 14:00:52 +0000

    n10773 solved

--- src/Input_output_add_etc/Sliver.java
+++ src/Input_output_add_etc/Sliver.java
@@ -819,6 +819,33 @@ public class Sliver {
         bw.close();
     }
 
+    public static void n10773() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int totalInputNum = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < totalInputNum; i++) {
+            int input = Integer.parseInt(br.readLine());
+            if (input == 0) {
+                stack.pop();
+            } else {
+                stack.push(input);
+            }
+        }
+
+        int sum = 0;
+        while (!stack.empty()) {
+            sum += stack.pop();
+        }
+        bw.write(Integer.toString(sum));
+        
+
+        bw.flush();
+        bw.close();
+    }
+
     
 }
 
