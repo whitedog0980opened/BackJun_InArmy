fc782d2d23245943175189ba2411813621934e11
whitedog0980opened <whitedog0980opened@gmail.com>
2025-11-14 12:54:55 +0000

    n14500 pro

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,115 @@
-import java.io.*;
-import java.util.*;
+import java.io.;
+import java.util.;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+public static void main(String[] args) throws IOException {
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int peopleNum = Integer.parseInt(br.readLine());
-
-        int[][] bodyData = new int[peopleNum][2];
-        for (int i = 0; i < peopleNum; i++) {
-            String[] inputStrs = br.readLine().split(" ");
-            int weight = Integer.parseInt(inputStrs[0]);
-            int height = Integer.parseInt(inputStrs[1]);
-
-            bodyData[i][0] = weight;
-            bodyData[i][1] = height;
+    //handle inputs
+    String mapSizeInput[] = br.readLine().split(" ");
+    int mapSizeX = Integer.parseInt(mapSizeInput[0]);
+    int mapSizeY = Integer.parseInt(mapSizeInput[1]);
+    //index 0 and n + 1 is blank value for border
+    int[][] map = new int[mapSizeY + 2][mapSizeX + 2]; //include border
+    //map index is start from 1
+    for (int i = 1; i < mapSizeY + 1; i++) {
+        String xInputLine[] = br.readLine().split(" ");
+        for (int j = 1; j < mapSizeX + 1; j++) {
+            int cellInput = Integer.parseInt(xInputLine[j - 1]);
+            map[i][j] = cellInput;
         }
-
-        int point[] = new int[peopleNum];
-        for (int i = 0; i < peopleNum; i++) {
-            for (int j = 0; j < peopleNum; j++) {
-                point[i] = (bodyData[i][0] < bodyData[j][0] 
-                            && bodyData[i][1] < bodyData[j][1]
-                            ) ? ++point[i] : point[i];
-            }
-        }
-
-        // //compare score
-        // int[] grade = new int[peopleNum];
-        // int crrGrade = 0;
-        // int sameStack = 0;
-        // int max = Integer.MAX_VALUE;
-        // for (int i = 0; i < peopleNum; i++) {
-        //     int maxI = 0;
-        //     int maxCrr = -1;
-        //     for (int j = 0; j < peopleNum; j++) {
-        //         if (maxCrr < point[j]) {
-        //             maxCrr = point[j];
-        //             maxI = j;
-        //         }
-        //     }
-        //     if (max > maxCrr) { //if max value down -> grade++
-        //         crrGrade += sameStack + 1;
-        //         max = maxCrr;
-        //         sameStack = 0;
-        //     } else {
-        //         sameStack++;
-        //     }
-        //     point[maxI] = -1;
-        //     grade[maxI] = crrGrade;
-        // }
-
-        boolean isFirst = true;
-        for (int i : point) {
-            if (isFirst) isFirst = false;
-            else bw.write(" ");
-            bw.write(Integer.toString(i + 1));
-        }
-        
-
-        bw.flush();
-        bw.close();
     }
+    //init map's border
+    for (int i = 0; i < mapSizeX + 2; i++) {
+        map[0][i] = -1;
+        map[mapSizeY + 1][i] = -1;
+    }
+    for (int i = 1; i < mapSizeY + 1; i++) {
+        map[i][0] = -1;
+        map[i][mapSizeX + 1] = -1;
+    }
+    // movement each tetra
+    int[][] tetraT = {
+        {1, 0},
+        {1, 1},
+        {2, 0}
+    };
+    int[][] tetraI = {
+        {1, 0},
+        {2, 0},
+        {3, 0}
+    };
+    int[][] tetraO = {
+        {1, 0},
+        {1, -1},
+        {0, -1}
+    };
+    int[][] tetraZ = {
+        {0, -1},
+        {1, -1},
+        {1, -2}
+    };
+    int[][] tetraL = {
+        {0, -1},
+        {0, -2},
+        {1, -2}
+    };
+
+    int max = -2;
+    //repeat for rotate
+    for (int i = 0; i < 4; i++) {
+        max = Math.max(max, tetraTry(tetraT, map, mapSizeX, mapSizeY, i));
+        max = Math.max(max, tetraTry(tetraI, map, mapSizeX, mapSizeY, i));
+        max = Math.max(max, tetraTry(tetraO, map, mapSizeX, mapSizeY, i));
+        max = Math.max(max, tetraTry(tetraZ, map, mapSizeX, mapSizeY, i));
+        max = Math.max(max, tetraTry(tetraL, map, mapSizeX, mapSizeY, i));
+    }
+
+    bw.write(Integer.toString(max));
+    
+
+    bw.flush();
+    bw.close();
+}
+
+public static int tetraTry(int[][] tetraShape, int[][] map, int mapSizeX, int mapSizeY, int deract) {
+    //deep copy tetraShape
+    int[][] rotatedTetraShape = new int[3][2];
+    for (int i = 0; i < 3; i++) {
+        rotatedTetraShape[i] = tetraShape[i].clone();
+    }
+    //rotate tetraShape
+    for (int i = 0; i < deract; i++) {
+        for (int j = 0; j < 3; j++) {
+            int temp = rotatedTetraShape[j][0];
+            rotatedTetraShape[j][0] = rotatedTetraShape[j][1];
+            rotatedTetraShape[j][1] = -temp;
+        }
+    }
+
+    //get sum
+    int max = -2;
+    for (int i = 0; i < mapSizeY + 1; i++) {
+        for (int j = 0; j < mapSizeX + 1; j++) {
+            int sum = map[i][j];
+            if (map[i][j] == -1)  {
+                sum = -1;
+                continue;
+            }
+            for (int[] indexs : rotatedTetraShape) {
+                int curI = i + indexs[1];
+                int curJ = j + indexs[0];
+                if (curI < 0 || curI >= map.length || curJ < 0 || curJ >= map[0].length || map[curI][curJ] == -1)  {
+                    sum = -1;
+                    continue;
+                };
+                sum += map[curI][curJ];
+            }
+            if (max < sum) max = sum;
+        }
+    }
+    return max; //-1 means every try is blocked by border
+
+
 }
