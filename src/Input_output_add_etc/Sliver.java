4c0b8c949d646618fafe8bc28f58c53848cbb1a3
whitedog0980opened0 <whitedog0980opened0@gmail.com>
2025-11-30 13:59:36 +0000

    n11651 solved

--- src/Input_output_add_etc/Sliver.java
+++ src/Input_output_add_etc/Sliver.java
@@ -923,6 +923,45 @@ public class Sliver {
         }
     }
 
+    static void n11651() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int nodeNum = Integer.parseInt(br.readLine());
+        List<Integer[]> nodes = new ArrayList<>();
+        for (int i = 0; i < nodeNum; i++) {
+            nodes.add(new Integer[2]);
+        }
+
+        for (int i = 0; i < nodeNum; i++)  {
+            String[] inputStrs = br.readLine().split(" ");
+            nodes.get(i)[0] = Integer.parseInt(inputStrs[0]);
+            nodes.get(i)[1] = Integer.parseInt(inputStrs[1]);
+        }
+
+        Collections.sort(nodes, (x, y) -> {
+            if (x[1] > y[1]) return 1;
+            else if (x[1] < y[1]) return -1;
+            else {
+                if (x[0] > y[0]) return 1;
+                else if (x[0] < y[0]) return -1;
+                else return 0;
+            }
+        });
+
+        //for mat form
+        bw.write(Integer.toString(nodes.get(0)[0]));
+        bw.write(" " + Integer.toString(nodes.get(0)[1]));
+        for (int i = 1; i < nodeNum; i++) {
+            bw.newLine();
+            bw.write(Integer.toString(nodes.get(i)[0]));
+            bw.write(" " + Integer.toString(nodes.get(i)[1]));
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
     
 }
 
--- src/Main.java
+++ src/Main.java
@@ -6,77 +6,39 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] inputStrs = br.readLine().split(" ");
-        int nodeNum = Integer.parseInt(inputStrs[0]);
-        int lineNum = Integer.parseInt(inputStrs[1]);
-        int startNode = Integer.parseInt(inputStrs[2]);
-
-        //input lines
-        int[][] lines = new int[lineNum][2];
-        for (int i = 0; i < lineNum; i++) {
-            String[] lineInputs = br.readLine().split(" ");
-            int[] line = {Integer.parseInt(lineInputs[0]), Integer.parseInt(lineInputs[1])};
-            lines[i] = line;
+        int nodeNum = Integer.parseInt(br.readLine());
+        List<Integer[]> nodes = new ArrayList<>();
+        for (int i = 0; i < nodeNum; i++) {
+            nodes.add(new Integer[2]);
         }
-        // create graph
-        LinkedList<Integer>[] graphs = new LinkedList[nodeNum + 1];
-        for (int i = 0; i < nodeNum + 1; i++) {
-            graphs[i] = new LinkedList<>();
+
+        for (int i = 0; i < nodeNum; i++)  {
+            String[] inputStrs = br.readLine().split(" ");
+            nodes.get(i)[0] = Integer.parseInt(inputStrs[0]);
+            nodes.get(i)[1] = Integer.parseInt(inputStrs[1]);
         }
-        for (int[] line : lines) {
-            graphs[line[0]].add(line[1]);
-            graphs[line[1]].add(line[0]);
-        }
-        for (LinkedList<Integer> graph : graphs) {
-            Collections.sort(graph);
-        }
-        
 
-        List<Integer> visited = new ArrayList<>();
-
-        boolean isFirst = true;
-        dfs(graphs, startNode, visited, bw, isFirst);
-        bw.newLine();
-
-        //init again
-        isFirst = true;
-        visited = new ArrayList<>();
-
-        Queue<Integer> queue = new LinkedList<Integer>();
-        queue.add(startNode);
-
-        visited.add(startNode);
-        bw.write(Integer.toString(startNode));
-        while (!queue.isEmpty()) {
-            int crrNode = queue.poll();
-
-            for (int i = 0; i < graphs[crrNode].size(); i++) {
-                int newNode = graphs[crrNode].get(i);
-                if (visited.contains(newNode)) continue;
-                else visited.add(newNode);
-                queue.add(newNode);
-                bw.write(" " + Integer.toString(newNode));
+        Collections.sort(nodes, (x, y) -> {
+            if (x[1] > y[1]) return 1;
+            else if (x[1] < y[1]) return -1;
+            else {
+                if (x[0] > y[0]) return 1;
+                else if (x[0] < y[0]) return -1;
+                else return 0;
             }
+        });
+
+        //for mat form
+        bw.write(Integer.toString(nodes.get(0)[0]));
+        bw.write(" " + Integer.toString(nodes.get(0)[1]));
+        for (int i = 1; i < nodeNum; i++) {
+            bw.newLine();
+            bw.write(Integer.toString(nodes.get(i)[0]));
+            bw.write(" " + Integer.toString(nodes.get(i)[1]));
         }
 
         bw.flush();
         bw.close();
     }
-
-    static void dfs(LinkedList<Integer>[] graphs, int crrNode, List<Integer> visited, BufferedWriter bw, boolean isFirst) throws IOException {
-        if (visited.contains(crrNode)) {
-            return;
-        } else {
-            visited.add(crrNode);
-            if (isFirst) {
-                bw.write(Integer.toString(crrNode));
-                isFirst = false;
-            }
-            else bw.write(" " + Integer.toString(crrNode));
-        }
-        for (int i = 0; i < graphs[crrNode].size(); i++) {
-            int nextNode = graphs[crrNode].get(i);
-            dfs(graphs, nextNode, visited, bw, isFirst);
-        }
-    }
 }
+
