073c04d07cd713d39ddb465fa6241a47786db4d5
whitedog0980opened0 <whitedog0980opened0@gmail.com>
2025-11-30 15:01:54 +0000

    n23291 WIP

--- src/Main.java
+++ src/Main.java
@@ -6,39 +6,93 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int nodeNum = Integer.parseInt(br.readLine());
-        List<Integer[]> nodes = new ArrayList<>();
-        for (int i = 0; i < nodeNum; i++) {
-            nodes.add(new Integer[2]);
+        //get inputs
+        String[] firstInputs = br.readLine().split(" ");
+        int fishbowlNum = Integer.parseInt(firstInputs[0]);
+        int minumunGap = Integer.parseInt(firstInputs[1]);
+        
+        String[] fishbowlStrInputs = br.readLine().split(" ");
+        int[][] fishbowlMap = new int[fishbowlNum][fishbowlNum]; //[y][x]좌표
+        //Empty place is MIN_VALUE
+        for (int[] i : fishbowlMap) {
+            Arrays.fill(i, Integer.MIN_VALUE);
+        }
+        //place first fishbowl
+        for (int i = 0; i < fishbowlNum; i++) {
+            int fishTemp = Integer.parseInt(fishbowlStrInputs[i]);
+            fishbowlMap[0][i] = fishTemp;
         }
 
-        for (int i = 0; i < nodeNum; i++)  {
-            String[] inputStrs = br.readLine().split(" ");
-            nodes.get(i)[0] = Integer.parseInt(inputStrs[0]);
-            nodes.get(i)[1] = Integer.parseInt(inputStrs[1]);
-        }
-
-        Collections.sort(nodes, (x, y) -> {
-            if (x[1] > y[1]) return 1;
-            else if (x[1] < y[1]) return -1;
-            else {
-                if (x[0] > y[0]) return 1;
-                else if (x[0] < y[0]) return -1;
-                else return 0;
+        while (!checkGap(minumunGap, fishbowlMap, fishbowlNum)) {
+            //add 1 for each minimum num
+            int minNum = Integer.MAX_VALUE
+            for (int i = 0; i < fishbowlNum; i++) { //find minimum num
+                int crrNum = fishbowlMap[0][i];
+                if (crrNum < minNum) minNum = crrNum;
             }
-        });
+            for (int i = 0; i < fishbowlNum; i++) { //add 1 on minimum num
+                if (fishbowlMap[0][i] == minNum) fishbowlMap[0][i]++;
+            }
 
-        //for mat form
-        bw.write(Integer.toString(nodes.get(0)[0]));
-        bw.write(" " + Integer.toString(nodes.get(0)[1]));
-        for (int i = 1; i < nodeNum; i++) {
-            bw.newLine();
-            bw.write(Integer.toString(nodes.get(i)[0]));
-            bw.write(" " + Integer.toString(nodes.get(i)[1]));
+            //stack 1st bowl on 2nd bowl
+            int[] tempFlatMap = Arrays.copyOf(fishbowlMap[0], fishbowlNum);
+            for (int i = 0; i < fishbowlNum - 1; i++) {
+                fishbowlMap[0][i] = fishbowlMap[0][i + 1];
+            }
+            fishbowlMap[0][fishbowlNum] = Integer.MIN_VALUE; // set lastbowl is empty
+            fishbowlMap[1][0] = tempFlatMap[0];
+
+
+
+
         }
+
+        
+
+        
 
         bw.flush();
         bw.close();
     }
+
+    static boolean checkGap(int minGap, int[][] fishbowlMap, int bowlNum) {
+        int minFishNum = Integer.MAX_VALUE;
+        int maxFishNum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < bowlNum; i++) { //check only 1st floor
+            int crrFishbowl = fishbowlMap[0][i];
+            if (crrFishbowl == Integer.MIN_VALUE) continue; //check empty
+            if (crrFishbowl > maxFishNum) {
+                maxFishNum = crrFishbowl;
+            }
+            if (crrFishbowl < minFishNum) {
+                minFishNum = crrFishbowl;
+            }
+        }
+        boolean result = (maxFishNum - minFishNum) <= minGap;
+        return result;
+    }
+
+    static void rollAndStack(int[][] fishbowlMap, int bowlNum) {
+        boolean rollable = true;
+        //floor
+        LinkedList<Integer> nonStackedFloor = new LinkedList<>();
+        for (int i : fishbowlMap[0]) {
+            nonStackedFloor.add(i);
+        }
+        nonStackedFloor.remove(); // first one is rolled
+        //roll box
+        int[][] rollingBox = new int[bowlNum][bowlNum];
+        rollingBox[0][0] = fishbowlMap[0][0];
+        rollingBox[0][1] = fishbowlMap[0][1];
+
+        while (rollable) {
+            int[][] newRollingBox = new int[bowlNum][bowlNum];
+            
+
+            //check rollable
+            int stacked num
+        }
+    } 
 }
 
