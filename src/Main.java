c06301699d5248e2f0a9d73ee41124e2b563749d
whitedog0980opened0 <whitedog0980opened0@gmail.com>
2025-12-01 14:50:48 +0000

    n23291 wip2

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
         int[][] fishbowlMap = new int[fishbowlNum][fishbowlNum]; //[y][x]좌표
         //Empty place is MIN_VALUE
         for (int[] i : fishbowlMap) {
-            Arrays.fill(i, Integer.MIN_VALUE);
+            Arrays.fill(i, 0);
         }
         //place first fishbowl
         for (int i = 0; i < fishbowlNum; i++) {
@@ -23,9 +23,10 @@ public class Main {
             fishbowlMap[0][i] = fishTemp;
         }
 
+        int tryCount = 0;
         while (!checkGap(minumunGap, fishbowlMap, fishbowlNum)) {
             //add 1 for each minimum num
-            int minNum = Integer.MAX_VALUE
+            int minNum = Integer.MAX_VALUE;
             for (int i = 0; i < fishbowlNum; i++) { //find minimum num
                 int crrNum = fishbowlMap[0][i];
                 if (crrNum < minNum) minNum = crrNum;
@@ -39,14 +40,19 @@ public class Main {
             for (int i = 0; i < fishbowlNum - 1; i++) {
                 fishbowlMap[0][i] = fishbowlMap[0][i + 1];
             }
-            fishbowlMap[0][fishbowlNum] = Integer.MIN_VALUE; // set lastbowl is empty
+            fishbowlMap[0][fishbowlNum - 1] = 0; // set lastbowl is empty
             fishbowlMap[1][0] = tempFlatMap[0];
 
-
-
-
+            fishbowlMap = rollAndStack(fishbowlMap, fishbowlNum);
+            moveFishes(fishbowlMap, fishbowlNum);
+            fishbowlMap = toFlatForm(fishbowlMap, fishbowlNum);
+            foldForm(fishbowlMap, fishbowlNum);
+            moveFishes(fishbowlMap, fishbowlNum);
+            foldForm(fishbowlMap, fishbowlNum);
+            tryCount++;
         }
 
+        bw.write(Integer.toString(tryCount));
         
 
         
@@ -61,7 +67,7 @@ public class Main {
 
         for (int i = 0; i < bowlNum; i++) { //check only 1st floor
             int crrFishbowl = fishbowlMap[0][i];
-            if (crrFishbowl == Integer.MIN_VALUE) continue; //check empty
+            if (crrFishbowl == 0) continue; //check empty
             if (crrFishbowl > maxFishNum) {
                 maxFishNum = crrFishbowl;
             }
@@ -73,7 +79,7 @@ public class Main {
         return result;
     }
 
-    static void rollAndStack(int[][] fishbowlMap, int bowlNum) {
+    static int[][] rollAndStack(int[][] fishbowlMap, int bowlNum) {
         boolean rollable = true;
         //floor
         LinkedList<Integer> nonStackedFloor = new LinkedList<>();
@@ -84,15 +90,112 @@ public class Main {
         //roll box
         int[][] rollingBox = new int[bowlNum][bowlNum];
         rollingBox[0][0] = fishbowlMap[0][0];
-        rollingBox[0][1] = fishbowlMap[0][1];
+        rollingBox[1][0] = fishbowlMap[1][0];
+
+        int boxSizeX = 1; // important
+        int boxSizeY = 2;
 
         while (rollable) {
             int[][] newRollingBox = new int[bowlNum][bowlNum];
-            
 
-            //check rollable
-            int stacked num
+            for (int i = 0; i < boxSizeX; i++) { // roll and float 1
+                for (int j = 0; j < boxSizeY; j++) {
+                    newRollingBox[boxSizeX - 1 - i + 1][j] = rollingBox[j][i];
+                }
+            }
+            if (boxSizeX < boxSizeY) boxSizeX++;
+            else boxSizeY++;
+
+            for (int i = 0; i < boxSizeX; i++) {
+                newRollingBox[0][i] = nonStackedFloor.removeFirst();
+            }
+
+            rollingBox = newRollingBox;
+
+            if (boxSizeY > nonStackedFloor.size()) {
+                rollable = false;
+            }
         }
+
+        for (int i = 0; i < nonStackedFloor.size(); i++) {
+            rollingBox[0][boxSizeX + i] = nonStackedFloor.removeFirst();
+        }
+        return rollingBox;
     } 
+
+    static void moveFishes(int[][] fishbowlMap, int bowlNum) {
+        int[][] addFishNumMap = new int[bowlNum][bowlNum];
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
+
+        for (int i = 0; i < bowlNum; i++) {
+            for (int j = 0; j < bowlNum; j++) {
+                if (fishbowlMap[j][i] == 0) continue;
+
+                for (int k = 0; k < 4; k++) {
+                    int di = i + dx[k];
+                    int dj = j + dy[k];
+                    if (di >= 0 && dj >= 0 && di < bowlNum && dj < bowlNum && fishbowlMap[dj][di] != 0) {
+                        int diff = Math.abs(fishbowlMap[j][i] - fishbowlMap[dj][di]) / 5;
+                        if (diff > 0) {
+                            if (fishbowlMap[j][i] > fishbowlMap[dj][di]) {
+                                addFishNumMap[j][i] -= diff;
+                                addFishNumMap[dj][di] += diff;
+                            } else {
+                                addFishNumMap[j][i] += diff;
+                                addFishNumMap[dj][di] -= diff;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < bowlNum; i++) {
+            for (int j = 0; j < bowlNum; j++) {
+                if (fishbowlMap[j][i] != 0) fishbowlMap[j][i] += addFishNumMap[j][i];
+            }
+        }
+    }
+
+    static int[][] toFlatForm(int[][] fishbowlMap, int bowlNum) {
+        LinkedList<Integer> flatMap = new LinkedList<>();
+        int dy = 0;
+        for (int i = 0; i < bowlNum; i++) {
+            if (fishbowlMap[0][i] == 0) break;
+            flatMap.add(fishbowlMap[0][i]);
+            int y = 1;
+            while (y < bowlNum && fishbowlMap[y][i] != 0) {
+                flatMap.add(fishbowlMap[y][i]);
+                y++;
+            }
+            dy = 1;
+        }
+
+        int[][] newFishBowlMap = new int[bowlNum][bowlNum];
+        for (int i = 0; i < flatMap.size(); i++) { 
+            newFishBowlMap[0][i] = flatMap.get(i);
+        }
+
+        return newFishBowlMap;
+    }
+
+    static void foldForm(int[][] fishbowlMap, int bowlNum) {
+        int halfBowlNum = bowlNum/2;
+        int quatBowlNum = bowlNum/4;
+        for (int i = 0; i < halfBowlNum; i++) {
+            fishbowlMap[1][i] = fishbowlMap[0][halfBowlNum - i];
+            fishbowlMap[0][halfBowlNum - i] = fishbowlMap[0][bowlNum - i - 1];
+            fishbowlMap[0][bowlNum - i - 1] = 0;
+        }
+        for (int i = 0; i < quatBowlNum; i++) {
+            fishbowlMap[2][i] = fishbowlMap[1][quatBowlNum - i];
+            fishbowlMap[1][quatBowlNum - i] = fishbowlMap[1][halfBowlNum - i];
+            fishbowlMap[1][halfBowlNum - i] = 0;
+            fishbowlMap[3][i] = fishbowlMap[0][quatBowlNum - i];
+            fishbowlMap[0][quatBowlNum - i] = fishbowlMap[0][halfBowlNum - i];
+            fishbowlMap[0][halfBowlNum - i] = 0;
+        }
+    }
 }
 
