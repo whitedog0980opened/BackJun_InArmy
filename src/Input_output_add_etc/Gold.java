3245445cefc72316b4e2dd8543bab5de7f55e490
whitedog0980opened0 <whitedog0980opened0@gmail.com>
2025-12-09 05:29:06 +0000

    n1074 solved

--- src/Input_output_add_etc/Gold.java
+++ src/Input_output_add_etc/Gold.java
@@ -314,4 +314,43 @@ public class Gold {
         }
         return max; //-1 means every try is blocked by border
     }
+    public static void n1074() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] inputs = br.readLine().split(" ");
+        int n = Integer.parseInt(inputs[0]);
+        int row = Integer.parseInt(inputs[1]);
+        int col = Integer.parseInt(inputs[2]);
+
+        int startNum = 0;
+        int endNum = (1 << (2 * n)) - 1; // (2^n)^2 - 1
+        int totalCellNum = endNum + 1;
+
+        int diviedBy = 1 << (n - 1);
+        int rDivied = row / diviedBy;
+        int rRest = row % diviedBy;
+        int cDivied = col / diviedBy;
+        int cRest = col % diviedBy;
+
+        while (true) {
+            startNum += rDivied * (totalCellNum / 2);
+            startNum += cDivied * (totalCellNum / 4);
+            totalCellNum /= 4;
+            endNum = startNum + totalCellNum - 1;
+
+            diviedBy /= 2;
+            if (diviedBy == 0) break;
+            rDivied = rRest / diviedBy;
+            rRest %= diviedBy;
+            cDivied = cRest / diviedBy;
+            cRest %= diviedBy;
+        }
+
+        bw.write(Integer.toString(endNum));
+
+
+        bw.flush();
+        bw.close();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,68 +6,37 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        //take first line = userNum, vectorNum
-        String[] firstInputLine = br.readLine().split(" ");
-        int userNum = Integer.parseInt(firstInputLine[0]);
-        int vectorNum = Integer.parseInt(firstInputLine[1]);
+        String[] inputs = br.readLine().split(" ");
+        int n = Integer.parseInt(inputs[0]);
+        int row = Integer.parseInt(inputs[1]);
+        int col = Integer.parseInt(inputs[2]);
 
-        //take vectors
-        LinkedList<Integer>[] vectors = new LinkedList[userNum + 1]; //array start for 1
-        for (int i = 1; i < userNum + 1; i++) { //init list
-            vectors[i] = new LinkedList<Integer>();
-        }
-        for (int i = 0; i < vectorNum; i++) { //fill vectors
-            String[] tempInput = br.readLine().split(" ");
-            int from = Integer.parseInt(tempInput[0]);
-            int to =  Integer.parseInt(tempInput[1]);
+        int startNum = 0;
+        int endNum = (1 << (2 * n)) - 1; // (2^n)^2 - 1
+        int totalCellNum = endNum + 1;
 
-            vectors[from].add(to);
-            vectors[to].add(from);
-        }
-        int[] kelvinNums = new int[userNum + 1];
+        int diviedBy = 1 << (n - 1);
+        int rDivied = row / diviedBy;
+        int rRest = row % diviedBy;
+        int cDivied = col / diviedBy;
+        int cRest = col % diviedBy;
 
-        for (int i = 1; i < userNum + 1; i++) { // main
-            Queue<Integer> queue = new LinkedList<>();
-            queue.add(i);
-            int[] deepCount = new int[userNum + 1]; //index = userNum, record deep
-            Arrays.fill(deepCount, Integer.MIN_VALUE);
-            deepCount[i] = 0;
-            //deep copy
-            LinkedList<Integer>[] copyedVectors = new LinkedList[userNum + 1];
-            for (int j = 1; j < userNum + 1; j++) {
-                copyedVectors[j] = new LinkedList<>(vectors[j]);
-            }
+        while (true) {
+            startNum += rDivied * (totalCellNum / 2);
+            startNum += cDivied * (totalCellNum / 4);
+            totalCellNum /= 4;
+            endNum = startNum + totalCellNum - 1;
 
-            while (!queue.isEmpty()) {
-                int crrNode = queue.remove();
-                int deepCounter = deepCount[crrNode] + 1;
-                for (int next : copyedVectors[crrNode]) {
-                    if (deepCount[next] == Integer.MIN_VALUE) { //isnt Visited?
-                        deepCount[next] = deepCounter;
-                        queue.add(next);
-                    } else {
-                        continue;
-                    }
-                }
-            }
-            int result = 1; //++1 because start point is -1
-            for (int j : deepCount) {
-                if (j < 0) continue;
-                result += j;
-            }
-
-            kelvinNums[i] = result; 
+            diviedBy /= 2;
+            if (diviedBy == 0) break;
+            rDivied = rRest / diviedBy;
+            rRest %= diviedBy;
+            cDivied = cRest / diviedBy;
+            cRest %= diviedBy;
         }
 
-        int min = Integer.MAX_VALUE;
-        int result = 0;
-        for (int i = userNum; i > 0; i--) {
-            if (kelvinNums[i] <= min) {
-                min = kelvinNums[i];
-                result = i;
-            }
-        }
-        bw.write(Integer.toString(result));
+        bw.write(Integer.toString(endNum));
+
 
         bw.flush();
         bw.close();
