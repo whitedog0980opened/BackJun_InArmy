797eb04f6b850a7ef39e9e04c347dd2131e0ddb1
whitedog0980opened0 <whitedog0980opened0@gmail.com>
2025-12-10 13:43:07 +0000

    n1927 solved

--- src/Input_output_add_etc/Sliver.java
+++ src/Input_output_add_etc/Sliver.java
@@ -1118,6 +1118,37 @@ public class Sliver {
         bw.flush();
         bw.close();
     }
+    static void n1927() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int trys = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        boolean isFirst = true;
+
+        for (int i = 0; i < trys; i++) {
+            int commend = Integer.parseInt(br.readLine());
+            if (commend == 0) {
+                int output = pq.isEmpty() ? 0 : pq.remove();
+                if (isFirst) {
+                    bw.write(Integer.toString(output));
+                    isFirst = false;
+                }
+                else {
+                    bw.newLine();
+                    bw.write(Integer.toString(output));
+                }
+            }
+            else {
+                pq.add(commend);
+            }
+
+        }
+
+        
+        bw.flush();
+        bw.close();
+    }
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,34 @@
 import java.io.*;
 import java.util.*;
-
+//https://testcase.ac/problems/1764
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int meetingNum = Integer.parseInt(br.readLine());
+        int trys = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        boolean isFirst = true;
 
-        int[][] meetings = new int[meetingNum][2];
-        for (int i = 0; i < meetingNum; i++) {
-            String[] inputs = br.readLine().split(" ");
-            int startTime = Integer.parseInt(inputs[0]);
-            int endTime = Integer.parseInt(inputs[1]);
+        for (int i = 0; i < trys; i++) {
+            int commend = Integer.parseInt(br.readLine());
+            if (commend == 0) {
+                int output = pq.isEmpty() ? 0 : pq.remove();
+                if (isFirst) {
+                    bw.write(Integer.toString(output));
+                    isFirst = false;
+                }
+                else {
+                    bw.newLine();
+                    bw.write(Integer.toString(output));
+                }
+            }
+            else {
+                pq.add(commend);
+            }
 
-            meetings[i][0] = startTime;
-            meetings[i][1] = endTime;
         }
 
-        Arrays.sort(meetings, (n1, n2) -> {
-            if (n1[1] == n2[1]) {
-                return Integer.compare(n1[0], n2[0]);
-            }
-            return Integer.compare(n1[1], n2[1]);
-        });
-
-
-
-        int leastStartTime = 0;
-        int ableMeetingNum = 0;
-        for (int i = 0; i < meetingNum; i++) {
-            int start = meetings[i][0];
-            int end = meetings[i][1];
-
-            // 이전에 선택한 회의와 겹치지 않으면 선택
-            if (start >= leastStartTime) {
-                ableMeetingNum++;
-                leastStartTime = end;
-            }
-        }
-
-        bw.write(Integer.toString(ableMeetingNum));
         
         bw.flush();
         bw.close();
