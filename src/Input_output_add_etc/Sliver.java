999b7c614682f697a25f4db43551a31b7076380e
whitedog0980opened0 <whitedog0980opened0@gmail.com>
2025-12-12 14:21:22 +0000

    n2579 solved

--- src/Input_output_add_etc/Sliver.java
+++ src/Input_output_add_etc/Sliver.java
@@ -1243,6 +1243,43 @@ public class Sliver {
         bw.flush();
         bw.close();
     }
+    static void n2579() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        //get inputs
+        int stairNum = Integer.parseInt(br.readLine());
+
+        int[] stairs = new int[stairNum];
+        int[][] stairsMax = new int[stairNum][2];
+        for (int i = 0; i < stairNum; i++) {
+            stairs[i] = Integer.parseInt(br.readLine());
+        }
+
+        if (stairNum == 1) {
+            bw.write(Integer.toString(stairs[0]));
+            bw.flush();
+            bw.close();
+            return;
+        }
+
+
+        stairsMax[0][0] = stairs[0];
+        stairsMax[1][0] = stairs[1];
+        stairsMax[1][1] = stairs[0] + stairs[1];
+
+        for (int i = 2; i < stairNum; i++)  {
+            stairsMax[i][1] = stairsMax[i - 1][0] + stairs[i];
+            stairsMax[i][0] =Math.max(stairsMax[i - 2][0], stairsMax[i - 2][1]) + stairs[i];
+        }
+
+        //int result = dfs(stairs, stairNum, 0, 0, -1);
+        int result = Math.max(stairsMax[stairNum - 1][0], stairsMax[stairNum - 1][1]);
+        bw.write(Integer.toString(result));
+
+        bw.flush();
+        bw.close();
+    }
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,44 @@
 import java.io.*;
 import java.util.*;
 //https://testcase.ac/problems/1764
+//https://www.acmicpc.net/step/16
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         //get inputs
-        String[] inputs = br.readLine().split(" ");
-        int rowNum = Integer.parseInt(inputs[0]);
-        int colNum = Integer.parseInt(inputs[1]);
-        int targerY = rowNum -1;
-        int targetX = colNum -1;
-        boolean[][] map = new boolean[rowNum][colNum];
+        int stairNum = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < rowNum; i++) {
-            String[] rowInputs = br.readLine().split("");
-
-            for (int j = 0; j < colNum; j++) {
-                boolean isRoad = Integer.parseInt(rowInputs[j]) == 1;
-                if (isRoad) map[i][j] = true;
-            }
+        int[] stairs = new int[stairNum];
+        int[][] stairsMax = new int[stairNum][2];
+        for (int i = 0; i < stairNum; i++) {
+            stairs[i] = Integer.parseInt(br.readLine());
         }
 
-        //BFS
-        Queue<int[]> queue = new LinkedList<>();
-        int[] startPoint = {0, 0, 0}; //x ,y ,depth
-        queue.add(startPoint);
-        HashSet<int[]> visited = new HashSet<>();
-        visited.add(startPoint);
-
-        int[] dx = {1, -1, 0 ,0};
-        int[] dy = {0, 0, 1, -1};
-        int maxDepth = 0;
-
-        first: while (!queue.isEmpty()) {
-            int[] crrPoint = queue.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int[] nextPoint = {crrPoint[0] + dx[i], crrPoint[1] + dy[i], crrPoint[2] + 1};
-                if (nextPoint[1] < 0 || nextPoint[1] >= rowNum ||
-                    nextPoint[0] < 0 || nextPoint[0] >= colNum ) {
-                    continue;
-                }
-                boolean isVisited = visited.stream().anyMatch(arr -> arr[0] == nextPoint[0] && arr[1] == nextPoint[1]);
-                if (isVisited) continue;
-
-                int[] target = {targetX, targerY};
-                int[] nextXY = {nextPoint[0], nextPoint[1]};
-                if(Arrays.equals(nextXY, target)) {
-                    maxDepth = nextPoint[2];
-                    break first;
-                }
-
-                if(map[nextPoint[1]][nextPoint[0]]) {
-                    queue.add(nextPoint);
-                    visited.add(nextPoint);
-                }
-            }
+        if (stairNum == 1) {
+            bw.write(Integer.toString(stairs[0]));
+            bw.flush();
+            bw.close();
+            return;
         }
 
-        bw.write(Integer.toString(maxDepth + 1));
-        
+
+        stairsMax[0][0] = stairs[0];
+        stairsMax[1][0] = stairs[1];
+        stairsMax[1][1] = stairs[0] + stairs[1];
+
+        for (int i = 2; i < stairNum; i++)  {
+            stairsMax[i][1] = stairsMax[i - 1][0] + stairs[i];
+            stairsMax[i][0] =Math.max(stairsMax[i - 2][0], stairsMax[i - 2][1]) + stairs[i];
+        }
+
+        //int result = dfs(stairs, stairNum, 0, 0, -1);
+        int result = Math.max(stairsMax[stairNum - 1][0], stairsMax[stairNum - 1][1]);
+        bw.write(Integer.toString(result));
+
         bw.flush();
         bw.close();
     }
+
 }
