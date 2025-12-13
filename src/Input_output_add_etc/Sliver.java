e585d50d5597f87d7a0d5fb43eb4e4116d4d08e0
whitedog0980opened0 <whitedog0980opened0@gmail.com>
2025-12-13 14:08:04 +0000

    n2630 solved

--- src/Input_output_add_etc/Sliver.java
+++ src/Input_output_add_etc/Sliver.java
@@ -1326,6 +1326,80 @@ public class Sliver {
         bw.flush();
         bw.close();
     }
+    static void n2630() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        //get inputs
+        int N = Integer.parseInt(br.readLine());
+        int foldableNum = Integer.numberOfTrailingZeros(N); //2의 몇승인가
+        boolean[][] paper = new boolean[N][N];
+        boolean[][] isChecked = new boolean[N][N];
+        int falseNum = 0;
+        int trueNum = 0;
+
+        boolean allSame1 = true; //all false
+        boolean allSame2 = true; //all true
+        for (int i = 0; i < N; i++) {
+            String[] inputLine = br.readLine().split(" ");
+            for (int j = 0; j < N; j++) {
+                boolean input = Integer.parseInt(inputLine[j]) == 1 ? false : true;
+                paper[j][i] = input;
+                if (input) allSame1 = false;
+                else allSame2 = false; 
+            }
+        }
+
+        if (allSame1) {
+            bw.write("0\n1");
+            bw.flush();
+            bw.close();
+            return;
+        } 
+        if (allSame2) {
+            bw.write("1\n0");
+            bw.flush();
+            bw.close();
+            return;
+        }
+
+        for (int i = 0; i < foldableNum; i++) {
+            int splitUnit = N / (1 << (i + 1));
+            boolean[] isIno = new boolean[1 << ((i + 1) * 2)];
+            Arrays.fill(isIno, true);
+            for (int j = 0; j < (1 << ((i + 1) * 2)); j++) {
+                int startPointX = (j % (1 << (i + 1))) * splitUnit;
+                int startPointY = (j / (1 << (i + 1))) * splitUnit; 
+                if (isChecked[startPointX][startPointY]) continue; //check isCounted paper
+
+                boolean color = paper[startPointX][startPointY];
+                for (int k = 0; k < splitUnit * splitUnit; k++) {
+                    int crrX = startPointX + k % splitUnit;
+                    int crrY = startPointY + k / splitUnit;
+                    if (paper[crrX][crrY] != color)  {
+                        isIno[j] = false;
+                        break;
+                    }
+                }
+                //marking if counted
+                if (isIno[j]) {// all paper was same color
+                    if (color) trueNum++;
+                    else falseNum++;
+                    for (int k = 0; k < Math.pow(splitUnit, 2); k++) {
+                        int crrX = startPointX + k % splitUnit;
+                        int crrY = startPointY + k / splitUnit;
+                        isChecked[crrX][crrY] = true;
+                    }
+                }
+
+            }
+        }
+
+        bw.write(Integer.toString(trueNum) + "\n" + Integer.toString(falseNum));
+
+        bw.flush();
+        bw.close();
+    }
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -9,44 +9,72 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         //get inputs
-        int computerNum = Integer.parseInt(br.readLine());
-        int graphNum = Integer.parseInt(br.readLine());
-        LinkedList<Integer>[] graphs = new LinkedList[computerNum + 1];
-        for (int i = 1; i < computerNum + 1; i++) { //init LinkedList Array
-            graphs[i] = new LinkedList<Integer>();
-        }
+        int N = Integer.parseInt(br.readLine());
+        int foldableNum = Integer.numberOfTrailingZeros(N); //2의 몇승인가
+        boolean[][] paper = new boolean[N][N];
+        boolean[][] isChecked = new boolean[N][N];
+        int falseNum = 0;
+        int trueNum = 0;
 
-        for (int i = 1; i < graphNum + 1; i++) {
-            String[] inputs = br.readLine().split(" ");
-            int from = Integer.parseInt(inputs[0]);
-            int to = Integer.parseInt(inputs[1]);
-            graphs[from].add(to);
-            graphs[to].add(from);
-        }
-
-        boolean[] visited = new boolean[computerNum + 1];
-        visited[1] = true;
-        
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(1);
-
-        int infected = 0;
-        while (!queue.isEmpty()) {
-            int crrPC = queue.poll();
-            for (int to : graphs[crrPC]) {
-                if (visited[to]) continue;
-                else {
-                    queue.add(to);
-                    visited[to] = true;
-                    infected++;
-                }
+        boolean allSame1 = true; //all false
+        boolean allSame2 = true; //all true
+        for (int i = 0; i < N; i++) {
+            String[] inputLine = br.readLine().split(" ");
+            for (int j = 0; j < N; j++) {
+                boolean input = Integer.parseInt(inputLine[j]) == 1 ? false : true;
+                paper[j][i] = input;
+                if (input) allSame1 = false;
+                else allSame2 = false; 
             }
         }
 
-        bw.write(Integer.toString(infected));
+        if (allSame1) {
+            bw.write("0\n1");
+            bw.flush();
+            bw.close();
+            return;
+        } 
+        if (allSame2) {
+            bw.write("1\n0");
+            bw.flush();
+            bw.close();
+            return;
+        }
 
+        for (int i = 0; i < foldableNum; i++) {
+            int splitUnit = N / (1 << (i + 1));
+            boolean[] isIno = new boolean[1 << ((i + 1) * 2)];
+            Arrays.fill(isIno, true);
+            for (int j = 0; j < (1 << ((i + 1) * 2)); j++) {
+                int startPointX = (j % (1 << (i + 1))) * splitUnit;
+                int startPointY = (j / (1 << (i + 1))) * splitUnit; 
+                if (isChecked[startPointX][startPointY]) continue; //check isCounted paper
 
-        
+                boolean color = paper[startPointX][startPointY];
+                for (int k = 0; k < splitUnit * splitUnit; k++) {
+                    int crrX = startPointX + k % splitUnit;
+                    int crrY = startPointY + k / splitUnit;
+                    if (paper[crrX][crrY] != color)  {
+                        isIno[j] = false;
+                        break;
+                    }
+                }
+                //marking if counted
+                if (isIno[j]) {// all paper was same color
+                    if (color) trueNum++;
+                    else falseNum++;
+                    for (int k = 0; k < Math.pow(splitUnit, 2); k++) {
+                        int crrX = startPointX + k % splitUnit;
+                        int crrY = startPointY + k / splitUnit;
+                        isChecked[crrX][crrY] = true;
+                    }
+                }
+
+            }
+        }
+
+        bw.write(Integer.toString(trueNum) + "\n" + Integer.toString(falseNum));
+
         bw.flush();
         bw.close();
     }
