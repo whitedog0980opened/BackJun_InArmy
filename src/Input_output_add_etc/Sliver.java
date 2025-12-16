d1b1af28ffdeb15a4b2c02f1e35904567543f4a0
whitedog0980opened0 <whitedog0980opened0@gmail.com>
2025-12-16 10:26:13 +0000

    n11399 solved

--- src/Input_output_add_etc/Sliver.java
+++ src/Input_output_add_etc/Sliver.java
@@ -1496,6 +1496,30 @@ public class Sliver {
         bw.flush();
         bw.close();
     }
+    static void n11399() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        //get inputs
+        int totalPeople = Integer.parseInt(br.readLine());
+        String[] timeInput = br.readLine().split(" ");
+        int[] times = new int[totalPeople]; 
+        for (int i = 0; i < totalPeople; i++) {
+            times[i] = Integer.parseInt(timeInput[i]);
+        }
+
+        Arrays.sort(times);
+        int sum = 0;
+        int sum2 = 0;
+        for (int i = 0; i < totalPeople; i++) {
+            sum += times[i];
+            sum2 += sum;
+        }
+        bw.write(Integer.toString(sum2));
+        
+        bw.flush();
+        bw.close();
+    }
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -9,35 +9,22 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         //get inputs
-        String[] firstInput = br.readLine().split(" ");
-        int treeNum = Integer.parseInt(firstInput[0]);
-        long needQuantity = Integer.parseInt(firstInput[1]);
-        String[] treesInput = br.readLine().split(" ");
-        int[] trees = new int[treeNum];
-        int longestTree = 0;
-        for (int i = 0; i < treeNum; i++) {
-            trees[i] = Integer.parseInt(treesInput[i]);
-            if (longestTree < trees[i]) longestTree = trees[i];
+        int totalPeople = Integer.parseInt(br.readLine());
+        String[] timeInput = br.readLine().split(" ");
+        int[] times = new int[totalPeople]; 
+        for (int i = 0; i < totalPeople; i++) {
+            times[i] = Integer.parseInt(timeInput[i]);
         }
 
-        long start = 0;
-        long end = longestTree;
-        long result= 0;
-        while (start <= end) {
-            long mid = start + (end - start) / 2;
-            long collected = 0;
-            for (int tree : trees) { //counting tree collected
-                if (tree > mid) collected += tree - mid;
-            }
-            long crrOverCollected = collected - needQuantity;
-            if (collected >= needQuantity) {
-                result = mid;
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
+        Arrays.sort(times);
+        int sum = 0;
+        int sum2 = 0;
+        for (int i = 0; i < totalPeople; i++) {
+            sum += times[i];
+            sum2 += sum;
         }
-        bw.write(Long.toString(result));
+        bw.write(Integer.toString(sum2));
+        
         bw.flush();
         bw.close();
     }
