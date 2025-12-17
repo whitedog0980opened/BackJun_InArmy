0c6dd9c192b515b894234aec5cae8ff618a11602
whitedog0980opened0 <whitedog0980opened0@gmail.com>
2025-12-17 13:47:51 +0000

    n9095 solved

--- src/Input_output_add_etc/Sliver.java
+++ src/Input_output_add_etc/Sliver.java
@@ -1520,6 +1520,37 @@ public class Sliver {
         bw.flush();
         bw.close();
     }
+    static void n9095() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] cases = new int[12];
+        cases[1] = 1;
+        cases[2] = 2;
+        cases[3] = 4;
+        //get inputs
+        int testCase = Integer.parseInt(br.readLine());
+        int max = 0;
+        int[] testCases = new int[testCase];
+        for (int i = 0; i < testCase; i++) {
+            int crr = Integer.parseInt(br.readLine());
+            testCases[i] = crr;
+            if (max < crr) max = crr;
+        }
+
+        for (int i = 4; i < max + 1; i++) {
+            cases[i] = cases[i - 1] + cases[i - 2] + cases[i - 3];
+        }
+
+        bw.write(Integer.toString(cases[testCases[0]]));
+        for (int i = 1; i < testCase; i++) {
+            bw.newLine();
+            bw.write(Integer.toString(cases[testCases[i]]));
+        }
+
+        bw.flush();
+        bw.close();
+    }
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -8,66 +8,30 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int[] cases = new int[12];
+        cases[1] = 1;
+        cases[2] = 2;
+        cases[3] = 4;
         //get inputs
-        String[] firstInputs = br.readLine().split(" ");
-        int M = Integer.parseInt(firstInputs[0]);
-        int N = Integer.parseInt(firstInputs[1]);
-
-        int[][] box = new int[N][M];
-        Queue<int[]> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[N][M];
-        
-        for (int i = 0; i < N; i++) {
-            String[] inputLine = br.readLine().split(" ");
-            for (int j = 0; j < M; j++) {
-                int crr = Integer.parseInt(inputLine[j]);
-                if (crr == 1) {
-                    visited[i][j] = true;
-                    int[] tomato = {i, j, 0};
-                    queue.add(tomato);
-                }
-                box[i][j] = Integer.parseInt(inputLine[j]);
-            }
+        int testCase = Integer.parseInt(br.readLine());
+        int max = 0;
+        int[] testCases = new int[testCase];
+        for (int i = 0; i < testCase; i++) {
+            int crr = Integer.parseInt(br.readLine());
+            testCases[i] = crr;
+            if (max < crr) max = crr;
         }
 
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
-        int result = 0;
-
-        while (!queue.isEmpty()) {
-            int[] crr = queue.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int nextN = crr[0] + dx[i];
-                int nextM = crr[1] + dy[i];
-                boolean checkBorder = (
-                    nextN >= 0 && nextN < N &&
-                    nextM >= 0 && nextM < M &&
-                    box[nextN][nextM] != -1 &&
-                    box[nextN][nextM] != 1 &&
-                    visited[nextN][nextM] == false
-                );
-
-                if (checkBorder) {
-                    int nextDepth = crr[2] + 1;
-                    if (nextDepth > result) result = nextDepth;
-                    int[] newTomamto = {nextN, nextM, nextDepth};
-                    queue.add(newTomamto);
-                    visited[nextN][nextM] = true;
-                }
-            }
+        for (int i = 4; i < max + 1; i++) {
+            cases[i] = cases[i - 1] + cases[i - 2] + cases[i - 3];
         }
 
-        //check left raw tomato
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (!visited[i][j] && box[i][j] == 0) {
-                    result = -1;
-                }
-            }
-        } 
+        bw.write(Integer.toString(cases[testCases[0]]));
+        for (int i = 1; i < testCase; i++) {
+            bw.newLine();
+            bw.write(Integer.toString(cases[testCases[i]]));
+        }
 
-        bw.write(Integer.toString(result));
         bw.flush();
         bw.close();
     }
