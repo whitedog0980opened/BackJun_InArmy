30251203ff3fd531f03a1fc8d1a5ba9e515771f3
whitedog0980opened0 <whitedog0980opened0@gmail.com>
2026-01-01 14:11:30 +0000

    n5525 solved

--- src/Input_output_add_etc/Sliver.java
+++ src/Input_output_add_etc/Sliver.java
@@ -1625,6 +1625,48 @@ public class Sliver {
         bw.flush();
         bw.close();
     }
+    static void n5525() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        String str2 = str.replace("IO", "1") + "O";
+
+        ArrayList<Integer> foundRepeatPattern = new ArrayList<>();
+        boolean[] is1 = {false};
+        int[] counter = {0};
+        str2.chars().forEach(i -> {
+            if (is1[0]) {
+                if (i == 'I') { // I
+                    foundRepeatPattern.add(counter[0]);
+                    counter[0] = 0;
+                    is1[0] = false;
+                } else if (i == 'O') { // 0
+                    foundRepeatPattern.add(counter[0] - 1);
+                    counter[0] = 0;
+                    is1[0] = false;
+                } else {
+                    counter[0]++;
+                }
+                
+            } else if(i == '1') { // 1
+                counter[0]++;
+                is1[0] = true;
+            }
+        });
+
+        int result = 0;
+        for (int i : foundRepeatPattern) {
+            int cul = i - N + 1;
+            if (cul > 0) result += cul;
+        }
+
+        bw.write(Integer.toString(result));
+        bw.flush();
+        bw.close();
+    }
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -10,86 +10,41 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int testCase = Integer.parseInt(br.readLine());
-        firstLoop : for (int i = 0; i < testCase; i++) {
-            boolean isFliped = false;
-            int[] deletingIndex = {0, 0};
-            String commends = br.readLine().replace("RR", "");
-            int arrayLength = Integer.parseInt(br.readLine());
-            String[] arrayString = br.readLine().replaceAll("[\\[\\]]", "").split(",");
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        String str2 = str.replace("IO", "1") + "O";
 
-            int[] array = new int[arrayLength];
-            for (int j = 0; j < arrayLength; j++) { //get array
-                array[j] = Integer.parseInt(arrayString[j]);
+        ArrayList<Integer> foundRepeatPattern = new ArrayList<>();
+        boolean[] is1 = {false};
+        int[] counter = {0};
+        str2.chars().forEach(i -> {
+            if (is1[0]) {
+                if (i == 'I') { // I
+                    foundRepeatPattern.add(counter[0]);
+                    counter[0] = 0;
+                    is1[0] = false;
+                } else if (i == 'O') { // 0
+                    foundRepeatPattern.add(counter[0] - 1);
+                    counter[0] = 0;
+                    is1[0] = false;
+                } else {
+                    counter[0]++;
+                }
+                
+            } else if(i == '1') { // 1
+                counter[0]++;
+                is1[0] = true;
             }
-            for (int j = 0; j < commends.length(); j++) { //handle commend
-                char crrCommend = commends.charAt(j);
-                if (crrCommend == 'D') {
-                    int aliveNumCounter = 0;
-                    for (int k : array) {
-                        if (k != -1) aliveNumCounter++;
-                    }
+        });
 
-                    if (aliveNumCounter >= 1) {
-                        if (!isFliped) {
-                            array[deletingIndex[0]++] = -1;
-                        } else {
-                            array[array.length - deletingIndex[1] - 1] = -1;
-                            deletingIndex[1]++;
-                        }
-
-                        // int[] newArray = new int[array.length - 1];
-                        // for (int k = 1; k < array.length; k++) {
-                        //     newArray[k - 1] = array[k];
-                        // }
-                        // array = newArray;
-                    }
-                    else {
-                        bw.write("error\n");
-                        continue firstLoop;
-                    }
-                }
-                else {
-                    int len = array.length;
-                    int[] newArray = new int[len];
-    
-                    for (int k = 0; k < len; k++) {
-                        newArray[k] = array[len - 1 - k];
-                    }
-    
-                    System.arraycopy(newArray, 0, array, 0, len);
-                    isFliped = !isFliped;
-                }
-            }
-            arrayLength = array.length;
-            int[] temp = new int[array.length];
-            int k = 0;
-            for (int j = 0; j < array.length; j++) {
-                if (array[j] != 0) {
-                    temp[k++] = array[j];
-                }
-            } 
-
-            if (arrayLength == 1) {
-                bw.write("[" + temp[0] + "]\n");
-                continue;
-            }
-            for (int j = 0; j < k; j++) {
-                if (j == k - 1) {
-                    bw.write(temp[j] + "]\n");
-                }
-                else if (j == 0) {
-                    bw.write("[" + temp[j] + ",");
-                }
-                else {
-                    bw.write(temp[j] + ",");
-                }
-            }
-
-
+        int result = 0;
+        for (int i : foundRepeatPattern) {
+            int cul = i - N + 1;
+            if (cul > 0) result += cul;
         }
-        
 
+        bw.write(Integer.toString(result));
         bw.flush();
         bw.close();
     }
