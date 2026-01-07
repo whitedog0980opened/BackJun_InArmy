6e40d12cda35da33a3c684dad8ce2ea662f5f428
whitedog0980opened0 <whitedog0980opened0@gmail.com>
2026-01-07 14:53:12 +0000

    n1167 wp

--- src/Main.java
+++ src/Main.java
@@ -8,91 +8,47 @@ import java.util.stream.IntStream;
 //need to review = 5430 ! deque
 
 public class Main {
-    static class MaxHeap {
-        int[] heap;
-        int capacity;
-        int next = 1;
-
-        MaxHeap(int capacity) {
-            heap = new int[capacity + 1];
-            this.capacity = capacity;
-
-        }
-
-        private void add(int x) {
-            // if heap full
-            if (next > capacity + 1) {
-                return;
-            }
-            int crr = next;
-            next++;
-            int parentIndex = crr / 2;
-            heap[crr] = x;
-            
-            while (parentIndex > 0) {
-                if (heap[parentIndex] < heap[crr]) {
-                    //exchange with parent
-                    int temp = heap[parentIndex];
-                    heap[parentIndex] = heap[crr];
-                    heap[crr] = temp;
-                    //set pr and crr
-                    crr = parentIndex;
-                    parentIndex = crr / 2;
-                } else {
-                    break;
-                }
-            }
-        }
-        private int pop() {
-            // if heap empty
-            if (next == 1) {
-                return 0;
-            }
-
-            int result = heap[1]; // 1 is root
-            heap[1] = heap[next - 1]; heap[next - 1] = 0;
-            next--;
-
-            int crr = 1;
-            int child = 2;
-            while (true) {
-                if (child > next - 1) {
-                    break;
-                }
-                if (capacity > child && heap[child] < heap[child + 1]) {
-                    child++;
-                }
-                if (heap[crr] < heap[child]) {
-                    int temp = heap[crr];
-                    heap[crr] = heap[child];
-                    heap[child] = temp;
-
-                    crr = child;
-                    child = child * 2;
-                    continue;
-                }
-                break;
-            }
-            return result;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int capacity = Integer.parseInt(br.readLine());
-        MaxHeap mp = new MaxHeap(capacity);
+        int nodeNum = Integer.parseInt(br.readLine());
 
-        for (int i = 0 ; i < capacity; i++) {
-            int crrNum = Integer.parseInt(br.readLine());
-            if (crrNum == 0) {
-                bw.write(Integer.toString(mp.pop()) + "\n");
-                continue;
+        //from = first index, to = sec index
+        int[][] map = new int[nodeNum + 1][nodeNum + 1]; 
+        // init map
+        for (int i = 0; i < nodeNum; i++) {
+            String[] inputs = br.readLine().split(" ");
+            int crr = 0;
+            int crrNode = Integer.parseInt(inputs[0]);
+            while (true) {
+                int nextNode = Integer.parseInt(inputs[++crr]);
+                if (nextNode == -1) break;
+                int length = Integer.parseInt(inputs[++crr]);
+                map[crrNode][nextNode] = length;
             }
-            mp.add(crrNum);
         }
-        
+
+        // i = crrNode
+        for (int i = 1; i < nodeNum + 1; i++) {
+            //this loop for find fromNum
+            int[] newConnection = new int[nodeNum + 1];
+            for (int j = 1; j < nodeNum + 1; j++) {
+                if (map[i][j] == 0) continue; //empty 
+
+                //this loop for find toNum and add new connection
+                for (int k = 1; k < nodeNum + 1; k++) {
+                    int fromNum = map[i][j];
+                    int toNum = map[fromNum][k];
+                    if (toNum == 0 || k == i) continue; //empty or fromNum and toNum endPoint is same 
+                    if (newConnection[k] == 0 || (newConnection[k] > toNum + fromNum)) {
+                        newConnection[k] = toNum + fromNum;
+                    }
+                }
+            }
+        }
+
+
         bw.flush();
         bw.close();
     }
