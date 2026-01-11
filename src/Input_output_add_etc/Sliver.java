9ff2c22788f6142ce0d6632587a25c581b4d74a8
whitedog0980opened0 <whitedog0980opened0@gmail.com>
2026-01-11 10:09:09 +0000

    n11726 solved

--- src/Input_output_add_etc/Sliver.java
+++ src/Input_output_add_etc/Sliver.java
@@ -1961,6 +1961,25 @@ public class Sliver {
         bw.flush();
         bw.close();
     }
+    static void n11726() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        long[] depth = new long[N + 3];
+        depth[1] = 1;
+        depth[2] = 1;
+        for (int i = 3; i < N + 1; i++) {
+            depth[i] = depth[i - 1] + depth[i - 2];
+            depth[i] %= 10007;
+        }
+        int result = (int) ((depth[N] + depth[N - 1]) % 10007);
+    
+        bw.write(Integer.toString(result));
+        
+        bw.flush();
+        bw.close();
+    }
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] infos = br.readLine().split(" ");
-        int nodeNum = Integer.parseInt(infos[0]);
+        int N = Integer.parseInt(br.readLine());
+        long[] depth = new long[N + 3];
+        depth[1] = 1;
+        depth[2] = 1;
+        for (int i = 3; i < N + 1; i++) {
+            depth[i] = depth[i - 1] + depth[i - 2];
+            depth[i] %= 10007;
+        }
+        int result = (int) ((depth[N] + depth[N - 1]) % 10007);
+    
+        bw.write(Integer.toString(result));
         
         bw.flush();
         bw.close();
