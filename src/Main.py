add98bdc0ddca89e5232016b257a3273e461fdeb
whitedog0980opened0 <whitedog0980opened0@gmail.com>
2026-01-14 13:57:32 +0000

    n11727py solved

--- src/Main.py
+++ src/Main.py
@@ -1,11 +1,14 @@
 import sys
 
-a = sys.stdin.readline().strip()
-b = sys.stdin.readline().strip()
-c = sys.stdin.readline().strip()
+n = int (sys.stdin.readline().strip());
 
-int_result = int(a) + int(b) - int(c);
-sys.stdout.write(str(int_result) + "\n");
+arr = [1, 3, 5, 11]
+if n < 5:
+    sys.stdout.write(str(arr[n - 1]))
+    sys.exit(0)
 
-str_result = str(int((a + b)) - int(c));
-sys.stdout.write(str(str_result))
+for i in range(4, n):
+    next_val = (arr[i-2] * 2 + arr[i - 1]) % 10007;
+    arr.append(next_val)
+
+print(arr[n - 1])
--- src/python_alg/Bronze.py
+++ src/python_alg/Bronze.py
@@ -10,4 +10,18 @@ str_result = str(int((a + b)) - int(c));
 sys.stdout.write(str(str_result))
 ###
 
+### n11727
+n = int (sys.stdin.readline().strip());
+
+arr = [1, 3, 5, 11]
+if n < 5:
+    sys.stdout.write(str(arr[n - 1]))
+    sys.exit(0)
+
+for i in range(4, n):
+    next_val = (arr[i-2] * 2 + arr[i - 1]) % 10007;
+    arr.append(next_val)
+
+print(arr[n - 1])
 ###
+
