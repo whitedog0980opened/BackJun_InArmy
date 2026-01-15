f4292b257c38f4cf280e24e306332a610a50560d
whitedog0980opened0 <whitedog0980opened0@gmail.com>
2026-01-15 01:23:23 +0000

    n1793py solved (from Temp)

---
+++ src/python_alg/Silver.py
@@ -0,0 +1,19 @@
+### 1793
+while True:
+    n = sys.stdin.readline().strip();
+    if not n:
+        break;
+    else:
+        n = int(n)
+    
+    arr = [1, 1, 3, 5, 11]
+    if n < 5:
+        sys.stdout.write(str(arr[n]) + "\n")
+        sys.stdout.flush()
+        continue;
+    for i in range(5, n + 1):
+        next_val = (arr[i-2] * 2 + arr[i - 1]);
+        arr.append(next_val)
+    sys.stdout.write(str(arr[n]) + "\n")
+    sys.stdout.flush()
+###
