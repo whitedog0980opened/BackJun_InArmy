0522601744ec12ae926c961c77313f6de6086c3c
whitedog0980opened0 <whitedog0980opened0@gmail.com>
2026-01-15 00:52:00 +0000

    n18111 solving

--- src/Main.java
+++ src/Main.java
@@ -15,67 +15,77 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int nodeNum = Integer.parseInt(st.nextToken());
-        int wayNum = Integer.parseInt(st.nextToken());
-        int partyNode = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int blocks = Integer.parseInt(st.nextToken());
 
-        //int[] -> index 0 = targetNode & index 1 = time to spend
-        ArrayList<int[]>[] ways = new ArrayList[nodeNum + 1];
-        ArrayList<int[]>[] reservedWays = new ArrayList[nodeNum + 1];
-        for (int i = 1; i < nodeNum + 1; i++) {
-            ways[i] = new ArrayList<int[]>();
-            reservedWays[i] = new ArrayList<int[]>();
-        }
-
-        for (int i = 0; i < wayNum; i++) {
+        int[] map = new int[n * m];
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            int fromNode = Integer.parseInt(st.nextToken());
-            int toNode = Integer.parseInt(st.nextToken());
-            int useTime = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < m; j++) {
+                map[i * m + j] = Integer.parseInt(st.nextToken());
+            }
+        } 
 
-            ways[fromNode].add(new int[]{toNode, useTime});
-            reservedWays[toNode].add(new int[]{fromNode, useTime});
+        int targetIndex = (int) Math.round((n - 1) * 2.0 / 3.0);
+        if (targetIndex > 256) targetIndex = 256;
+        int needBlocks = 0;
+        int spentTime = 0;
+
+        Arrays.sort(map);
+
+        //cut over targerIndex
+        for (int i = 0; i < n * m; i++) {
+            if (map[i] > targetIndex) {
+                int toBrakeBlocks = map[i] - targetIndex;
+                blocks += toBrakeBlocks;
+                spentTime += toBrakeBlocks * 2;
+                map[i] = toBrakeBlocks;
+            }
+            else if (map[i] < targetIndex) {
+                int toFillBlocks = targetIndex - map[i];
+                needBlocks += toFillBlocks;
+            }
         }
 
-        int longestTime = 0;
-
-        int[] wayToParty = bfs_1238(partyNode, nodeNum, ways);
-        int[] wayToHome = bfs_1238(partyNode, nodeNum, reservedWays);
-        for (int i = 1; i < nodeNum + 1; i++) {
-            int totalTime = wayToParty[i] + wayToHome[i];
-            if (totalTime > longestTime) longestTime = totalTime;
+        int prevTargetValue = map[targetIndex - 1];
+        int crrTarget = map[targetIndex] - 1;
+        int toBrakeBlocks = crrTarget + 1;
+        int toFillBlocks = n * m - crrTarget - 1;
+        //if don't have enough blocks
+        while (needBlocks > blocks) {
+            if (crrTarget == prevTargetValue) {
+                targetIndex -= 1;
+                prevTargetValue = map[targetIndex - 1];
+                toBrakeBlocks += 1;
+                toFillBlocks -= 1;
+            }
+            if (toFillBlocks > blocks) {
+                crrTarget--;
+                blocks += toBrakeBlocks;
+                needBlocks -= toFillBlocks;
+            }
         }
 
-        bw.write(Integer.toString(longestTime));
+        //fill
+        for (int i = 0; i < n * m; i++) {
+            if (crrTarget > map[i]) {
+                int toFill = crrTarget - map[i];
+                spentTime += toFill;
+            }
+            else if (crrTarget < map[i]) {
+                int toBrack = map[i] - crrTarget;
+                spentTime += toBrack * 2;
+            }
+        }
+
+        bw.write(Integer.toString(spentTime) + " " + Integer.toString(crrTarget));
+
+
+
 
          
         bw.flush();
         bw.close();
-    }
-    static int[] bfs_1238(int targetNode, int nodeNum, ArrayList<int[]>[] ways) {
-        int[] disk = new int[nodeNum + 1];
-        Arrays.fill(disk, Integer.MAX_VALUE);
-        disk[targetNode] = 0;
-        
-        PriorityQueue<int[]> queue = new PriorityQueue<>((n1, n2)-> Integer.compare(n1[1], n2[1]));
-        int[] startNode = {targetNode, 0};
-        queue.add(startNode);
-        
-        
-        while (!queue.isEmpty()) {
-            int[] crrNode = queue.poll();
-            
-            if (crrNode[1] > disk[crrNode[0]]) continue;
-
-            for (int[] i : ways[crrNode[0]]) {
-                //check this way is shotter then disk
-                int nextTime = i[1] + crrNode[1];
-                if (disk[i[0]] <= nextTime) continue;
-                int[] nextNode = {i[0], nextTime};
-                disk[nextNode[0]] = nextTime;
-                queue.add(nextNode);
-            }
-        }
-        return disk;
     }
 }
