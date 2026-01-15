505e9387969db46be8e264b2295e61e64ee9808d
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-01-15 14:21:41 +0000

    n18111j solved

--- src/Input_output_add_etc/slivers/Sliver2.java
+++ src/Input_output_add_etc/slivers/Sliver2.java
@@ -70,4 +70,57 @@ public class Sliver2{
         bw.flush();
         bw.close();
     }
+    static void n18111() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int blocks = Integer.parseInt(st.nextToken());
+
+        int maxHeight = 0;
+        //init map
+        int[] map = new int[n * m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                int crr = Integer.parseInt(st.nextToken());
+                map[i * m + j] = crr;
+
+                maxHeight = Math.max(maxHeight, crr);
+            }
+        }
+        
+        Arrays.sort(map);
+        int startHeight = maxHeight; //start from highest y
+        int minimumTime = Integer.MAX_VALUE;
+        int y = 0;
+        for (int crrHeight = startHeight; crrHeight >= 0; crrHeight--) {
+            int needBlocks = 0;
+            int spentTime = 0;
+            //check has enough blocks
+            for (int j : map) {
+                int diff = Math.abs(j - crrHeight);
+                if (j < crrHeight) {
+                    needBlocks += diff;
+                    spentTime += diff;
+                }
+                else if (j > crrHeight) {
+                    needBlocks -= diff;
+                    spentTime += diff * 2;
+                }
+            }
+            boolean hasEnough = needBlocks <= blocks;
+            if (hasEnough && minimumTime > spentTime) {
+                minimumTime = spentTime;
+                y = crrHeight;
+            }
+        }
+
+
+        bw.write(Integer.toString(minimumTime) + " " + Integer.toString(y));
+        bw.flush();
+        bw.close();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,6 @@ import java.util.stream.IntStream;
 //want to study : hashTable
 //need to review = 5430 ! deque
 
-
-// Dijkstra <- 이거 이용하라는데 공부 필요할듯.
-// 1 3 6
-// 0 256 0
-// 512 0
-// 512 87
-// 1 3 18
-// 0 256 0
-// 512 0
-// 512 91
-// 리펙토링 필요
-//n = 1. m = 1 일때 51에서 인덱스 -1 오류 발생
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,82 +17,47 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
         int blocks = Integer.parseInt(st.nextToken());
 
+        int maxHeight = 0;
+        //init map
         int[] map = new int[n * m];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                map[i * m + j] = Integer.parseInt(st.nextToken());
-            }
-        } 
+                int crr = Integer.parseInt(st.nextToken());
+                map[i * m + j] = crr;
 
-        int targetIndex = (int) Math.round((n * m - 1) * 2.0 / 3.0);
-        if (targetIndex > 256) targetIndex = 256;
-        int needBlocks = 0;
-        int spentTime = 0;
-
-        Arrays.sort(map);
-
-        //cut over targerIndex
-        for (int i = 0; i < n * m; i++) {
-            if (map[i] > map[targetIndex]) {
-                int toBrakeBlocks = map[i] - map[targetIndex];
-                blocks += toBrakeBlocks;
-                spentTime += toBrakeBlocks * 2;
-                map[i] -= toBrakeBlocks;
-            }
-            else if (map[i] < map[targetIndex]) {
-                int toFillBlocks = map[targetIndex] - map[i];
-                needBlocks += toFillBlocks;
+                maxHeight = Math.max(maxHeight, crr);
             }
         }
-
-        int prevTargetValue = map[targetIndex - 1];
-        int crrTarget = map[targetIndex];
-        int toBrakeBlocks = 0;
-        for (int i = 0; i < n * m; i++) {
-            if (map[i] == map[targetIndex]) {
-                toBrakeBlocks++;
-            }
-        } 
-        int toFillBlocks = n * m - toBrakeBlocks;
-        //if don't have enough blocks
-        while (crrTarget >= 0) {
-            long currentInventory = blocks;
-            long currentNeedBlocks = 0;
-            long currentSpentTime = 0;
-
-            for (int i = 0; i < n * m; i++) {
-                if (map[i] > crrTarget) {
-                    int toBrake = map[i] - crrTarget;
-                    currentInventory += toBrake;
-                    currentSpentTime += toBrake * 2L;
-                } else if (map[i] < crrTarget) {
-                    int toFill = crrTarget - map[i];
-                    currentNeedBlocks += toFill;
-                    currentSpentTime += toFill * 1L;
+        
+        Arrays.sort(map);
+        int startHeight = maxHeight; //start from highest y
+        int minimumTime = Integer.MAX_VALUE;
+        int y = 0;
+        for (int crrHeight = startHeight; crrHeight >= 0; crrHeight--) {
+            int needBlocks = 0;
+            int spentTime = 0;
+            //check has enough blocks
+            for (int j : map) {
+                int diff = Math.abs(j - crrHeight);
+                if (j < crrHeight) {
+                    needBlocks += diff;
+                    spentTime += diff;
+                }
+                else if (j > crrHeight) {
+                    needBlocks -= diff;
+                    spentTime += diff * 2;
                 }
             }
-
-            if (currentInventory >= currentNeedBlocks) {
-                break;
-            }
-
-            crrTarget--;
-        }
-
-        //fill
-        for (int i = 0; i < n * m; i++) {
-            if (crrTarget > map[i]) {
-                int toFill = crrTarget - map[i];
-                spentTime += toFill;
-            }
-            else if (crrTarget < map[i]) {
-                int toBrack = map[i] - crrTarget;
-                spentTime += toBrack * 2;
+            boolean hasEnough = needBlocks <= blocks;
+            if (hasEnough && minimumTime > spentTime) {
+                minimumTime = spentTime;
+                y = crrHeight;
             }
         }
 
-        bw.write(Integer.toString(spentTime) + " " + Integer.toString(crrTarget));
+
+        bw.write(Integer.toString(minimumTime) + " " + Integer.toString(y));
         bw.flush();
         bw.close();
     }
