4984d16c0edb9d93bd8f99b77c47e4a4666e8293
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-01-16 11:13:52 +0000

    n9625py stocked

--- src/Main.py
+++ src/Main.py
@@ -1,10 +1,15 @@
 import sys
-n = int(sys.stdin.readline().strip());
 
-my_set = set();
-for i in range(n):
-    my_set.add(sys.stdin.readline().strip());
+push_times = int(sys.stdin.readline().strip())
 
-result = n - len(my_set)
-print(result);
+ab_list = [[1, 0], [0, 1], [1, 1]]
+if push_times < 4:
+    print(ab_list[push_times - 1][0], ab_list[push_times - 1][1])
+
+for i in range(3, push_times + 1):
+    new_a = ab_list[i - 1][1]
+    new_b = ab_list[i - 1][0] + ab_list[i - 1][1]
+    ab_list.append([new_a, new_b]);
+        
+print(ab_list[push_times][0], ab_list[push_times][1])
     
