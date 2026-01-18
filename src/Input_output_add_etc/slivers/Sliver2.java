584766330cf4a86bf6eaa9132e22d61a56b42199
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-01-18 13:44:15 +0000

    n14405 solved

--- src/Input_output_add_etc/slivers/Sliver2.java
+++ src/Input_output_add_etc/slivers/Sliver2.java
@@ -123,4 +123,43 @@ public class Sliver2{
         bw.flush();
         bw.close();
     }
+    static void n14405() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char[] pikaSaid = br.readLine().toCharArray();
+
+        int size = pikaSaid.length;
+        boolean isCorrect = true;
+        for (int i = 0; i < size; i++) { 
+            //borderCheck
+            if (i + 1 >= size) {
+                isCorrect = false;
+                break;
+            }
+            if (pikaSaid[i] == 'p' && pikaSaid[i + 1] == 'i') {
+                i++;
+                continue;
+            }
+            if (pikaSaid[i] == 'k' && pikaSaid[i + 1] == 'a') {
+                i++;
+                continue;
+            }
+            if (i + 2 >= size) {
+                isCorrect = false;
+                break;
+            }
+            if (pikaSaid[i] == 'c' && pikaSaid[i + 1] == 'h' && pikaSaid[i + 2] == 'u') {
+                i += 2;
+                continue;
+            }
+            isCorrect = false;
+            break;
+        }
+
+        if (isCorrect) bw.write("YES");
+        else bw.write("NO");
+        bw.flush();
+        bw.close();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -12,52 +12,38 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int blocks = Integer.parseInt(st.nextToken());
+        char[] pikaSaid = br.readLine().toCharArray();
 
-        int maxHeight = 0;
-        //init map
-        int[] map = new int[n * m];
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                int crr = Integer.parseInt(st.nextToken());
-                map[i * m + j] = crr;
-
-                maxHeight = Math.max(maxHeight, crr);
+        int size = pikaSaid.length;
+        boolean isCorrect = true;
+        for (int i = 0; i < size; i++) { 
+            //borderCheck
+            if (i + 1 >= size) {
+                isCorrect = false;
+                break;
             }
-        }
-        
-        Arrays.sort(map);
-        int startHeight = maxHeight; //start from highest y
-        int minimumTime = Integer.MAX_VALUE;
-        int y = 0;
-        for (int crrHeight = startHeight; crrHeight >= 0; crrHeight--) {
-            int needBlocks = 0;
-            int spentTime = 0;
-            //check has enough blocks
-            for (int j : map) {
-                int diff = Math.abs(j - crrHeight);
-                if (j < crrHeight) {
-                    needBlocks += diff;
-                    spentTime += diff;
-                }
-                else if (j > crrHeight) {
-                    needBlocks -= diff;
-                    spentTime += diff * 2;
-                }
+            if (pikaSaid[i] == 'p' && pikaSaid[i + 1] == 'i') {
+                i++;
+                continue;
             }
-            boolean hasEnough = needBlocks <= blocks;
-            if (hasEnough && minimumTime > spentTime) {
-                minimumTime = spentTime;
-                y = crrHeight;
+            if (pikaSaid[i] == 'k' && pikaSaid[i + 1] == 'a') {
+                i++;
+                continue;
             }
+            if (i + 2 >= size) {
+                isCorrect = false;
+                break;
+            }
+            if (pikaSaid[i] == 'c' && pikaSaid[i + 1] == 'h' && pikaSaid[i + 2] == 'u') {
+                i += 2;
+                continue;
+            }
+            isCorrect = false;
+            break;
         }
 
-
-        bw.write(Integer.toString(minimumTime) + " " + Integer.toString(y));
+        if (isCorrect) bw.write("YES");
+        else bw.write("NO");
         bw.flush();
         bw.close();
     }
