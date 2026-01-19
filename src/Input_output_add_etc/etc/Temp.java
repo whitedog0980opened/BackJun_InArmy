53ccaf7627dc30fe86c6a72c932203be6a74f35c
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-01-19 14:49:08 +0000

    n18870 solved from temp

--- src/Input_output_add_etc/etc/Temp.java
+++ src/Input_output_add_etc/etc/Temp.java
@@ -20,40 +20,6 @@ import java.util.StringTokenizer;
 import java.util.TreeSet;
 
 public class Temp {
-    static void n18870() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int nums = Integer.parseInt(br.readLine());
-        //this array is key of Hashmap
-        int[] inputNums = new int[nums]; // original input data
-
-        TreeSet<Integer> treeSet = new TreeSet<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-    
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < nums; i++) {
-            int x = Integer.parseInt(st.nextToken());
-            treeSet.add(x);
-            inputNums[i] = x;
-        }
-
-        int treeSizeSave = treeSet.size();
-        for (int i = 0; i < treeSizeSave; i++) {
-            map.put(treeSet.pollFirst(), i);
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < nums; i++) {
-            int index = map.get(inputNums[i]);
-            sb.append(index + " ");
-        }
-        String result = sb.toString().trim();
-        bw.write(result);
-         
-        bw.flush();
-        bw.close();
-    }
 
     static void n21736() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
--- src/Main.java
+++ src/Main.java
@@ -12,33 +12,52 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        char[] chars = br.readLine().toCharArray();
-        int[] indexOfChars = new int[chars.length];
-        for (int i = 0; i < chars.length; i++) {
-            indexOfChars[i] = i;
+        int tc = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < tc; i++) {
+            char[] chars = br.readLine().toCharArray();
+            Arrays.sort(chars);
+            int[] indexOfChars = new int[chars.length];
+            for (int j = 0; j < chars.length; j++) {
+                indexOfChars[j] = j;
+            }
+            if (i == 0) {
+                bw.write("Case # " + Integer.toString(i + 1) + ":");
+            } else {
+                bw.write("\nCase # " + Integer.toString(i + 1) + ":");
+            }
+            dfs14534(0, indexOfChars, chars, bw, list);
         }
-
-
-
-
-        
-
 
         bw.flush();
         bw.close();
     }
-    static void dfs(int depth, int[] arr, char[] chars, BufferedWriter bw) {
+    static void dfs14534(int depth, int[] arr, char[] chars, BufferedWriter bw, ArrayList<Integer> list) throws IOException {
         if (depth == arr.length) {
             for (int i = 0; i < arr.length; i++) {
-                char a = chars[arr[i]];
-                bw.write(Character.toString(a));
+                list.add(arrToInt(arr));
             }
-
-            
-            
+            return;
         }
 
-
-        
+        for (int i = depth; i < arr.length; i++) {
+            swap14534(depth, i, arr);
+            dfs14534(depth + 1, arr, chars, bw, list);
+            swap14534(depth, i, arr);
+        }
+    }
+    static void swap14534(int i, int j, int[] arr) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    static int arrToInt(int[] arr) {
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            result += arr[i] * (int) Math.pow(10 ,arr.length - i - 1);
+        }
+        return result;
     }
 }
