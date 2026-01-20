dd16dc7bcb2945c5bb7d2d9c3baad32768b41d0f
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-01-20 13:47:54 +0000

    n14534 solved

--- src/Input_output_add_etc/slivers/Sliver2.java
+++ src/Input_output_add_etc/slivers/Sliver2.java
@@ -289,4 +289,92 @@ public class Sliver2{
         bw.flush();
         bw.close();
     }
+    static void n14534() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int tc = Integer.parseInt(br.readLine());
+
+        HashSet<Integer> list = new HashSet<>();
+        ArrayList<Integer>[] patterns = new ArrayList[6];
+
+        for (int i = 0; i < tc; i++) {
+            char[] chars = br.readLine().toCharArray();
+
+            //check pattern exist
+            int maxDepth = chars.length;
+            if (patterns[maxDepth] != null) {
+                printCase14534(chars, patterns[maxDepth], i, bw);
+                continue;
+            }
+
+            int[] indexOfChars = new int[chars.length];
+            for (int j = 0; j < chars.length; j++) {
+                indexOfChars[j] = j;
+            }
+            
+            dfs14534(0, indexOfChars, chars, list);
+            ArrayList<Integer> sortedList = new ArrayList<>(list);
+            Collections.sort(sortedList);
+            patterns[maxDepth] = sortedList;
+            printCase14534(chars, patterns[maxDepth], i, bw);
+            list = new HashSet<>();
+        }
+
+        bw.flush();
+        bw.close();
+    }
+    static void dfs14534(int depth, int[] arr, char[] chars, HashSet<Integer> list) {
+        if (depth == arr.length) {
+            for (int i = 0; i < arr.length; i++) {
+                list.add(arrToInt14534(arr));
+            }
+            return;
+        }
+
+        for (int i = depth; i < arr.length; i++) {
+            swap14534(depth, i, arr);
+            dfs14534(depth + 1, arr, chars, list);
+            swap14534(depth, i, arr);
+        }
+    }
+    static void swap14534(int i, int j, int[] arr) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    static int arrToInt14534(int[] arr) {
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            result += arr[i] * (int) Math.pow(10 ,arr.length - i - 1);
+        }
+        return result;
+    }
+    static void printCase14534(char[] chars, ArrayList<Integer> pattern, int testcase, BufferedWriter bw) throws IOException {
+        if (testcase == 0) {
+            bw.write("Case # " + Integer.toString(testcase + 1) + ":");
+        } else {
+            bw.write("\nCase # " + Integer.toString(testcase + 1) + ":");
+        }
+        //testCodes
+        // bw.newLine();
+        // for (int temp : pattern) {
+        //     bw.write(Integer.toString(temp) + ", ");
+        // }
+        // bw.newLine();
+        // for (char temp : chars) {
+        //     bw.write(Character.toString(temp) + ", ");
+        // }
+        int charNum = chars.length;
+        for (int singlePtn : pattern) {
+            bw.newLine();
+
+            int pre = 0;
+            for (int i = charNum - 1; i >= 0; i--) {
+                int crr = (singlePtn / (int) Math.pow(10, i)) - pre ;
+                bw.write(Character.toString(chars[crr]));
+                pre = pre * 10 + crr * 10;
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -18,37 +18,46 @@ public class Main {
 
         int tc = Integer.parseInt(br.readLine());
 
-        ArrayList<Integer> list = new ArrayList<>();
+        HashSet<Integer> list = new HashSet<>();
+        ArrayList<Integer>[] patterns = new ArrayList[6];
 
         for (int i = 0; i < tc; i++) {
             char[] chars = br.readLine().toCharArray();
-            Arrays.sort(chars);
+
+            //check pattern exist
+            int maxDepth = chars.length;
+            if (patterns[maxDepth] != null) {
+                printCase14534(chars, patterns[maxDepth], i, bw);
+                continue;
+            }
+
             int[] indexOfChars = new int[chars.length];
             for (int j = 0; j < chars.length; j++) {
                 indexOfChars[j] = j;
             }
-            if (i == 0) {
-                bw.write("Case # " + Integer.toString(i + 1) + ":");
-            } else {
-                bw.write("\nCase # " + Integer.toString(i + 1) + ":");
-            }
-            dfs14534(0, indexOfChars, chars, bw, list);
+            
+            dfs14534(0, indexOfChars, chars, list);
+            ArrayList<Integer> sortedList = new ArrayList<>(list);
+            Collections.sort(sortedList);
+            patterns[maxDepth] = sortedList;
+            printCase14534(chars, patterns[maxDepth], i, bw);
+            list = new HashSet<>();
         }
 
         bw.flush();
         bw.close();
     }
-    static void dfs14534(int depth, int[] arr, char[] chars, BufferedWriter bw, ArrayList<Integer> list) throws IOException {
+    static void dfs14534(int depth, int[] arr, char[] chars, HashSet<Integer> list) {
         if (depth == arr.length) {
             for (int i = 0; i < arr.length; i++) {
-                list.add(arrToInt(arr));
+                list.add(arrToInt14534(arr));
             }
             return;
         }
 
         for (int i = depth; i < arr.length; i++) {
             swap14534(depth, i, arr);
-            dfs14534(depth + 1, arr, chars, bw, list);
+            dfs14534(depth + 1, arr, chars, list);
             swap14534(depth, i, arr);
         }
     }
@@ -57,11 +66,41 @@ public class Main {
         arr[i] = arr[j];
         arr[j] = temp;
     }
-    static int arrToInt(int[] arr) {
+    static int arrToInt14534(int[] arr) {
         int result = 0;
         for (int i = 0; i < arr.length; i++) {
             result += arr[i] * (int) Math.pow(10 ,arr.length - i - 1);
         }
         return result;
+    }
+    static void printCase14534(char[] chars, ArrayList<Integer> pattern, int testcase, BufferedWriter bw) throws IOException {
+        if (testcase == 0) {
+            bw.write("Case # " + Integer.toString(testcase + 1) + ":");
+        } else {
+            bw.write("\nCase # " + Integer.toString(testcase + 1) + ":");
+        }
+
+        //testCodes
+        // bw.newLine();
+        // for (int temp : pattern) {
+        //     bw.write(Integer.toString(temp) + ", ");
+        // }
+        // bw.newLine();
+        // for (char temp : chars) {
+        //     bw.write(Character.toString(temp) + ", ");
+        // }
+        
+
+        int charNum = chars.length;
+        for (int singlePtn : pattern) {
+            bw.newLine();
+
+            int pre = 0;
+            for (int i = charNum - 1; i >= 0; i--) {
+                int crr = (singlePtn / (int) Math.pow(10, i)) - pre ;
+                bw.write(Character.toString(chars[crr]));
+                pre = pre * 10 + crr * 10;
+            }
+        }
     }
 }
