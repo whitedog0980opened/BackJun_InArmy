312c464a7fcc2afc6178f8e4073db0e0b2d628ac
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-01-22 13:49:20 +0000

    n24860py solved

--- src/python_alg/Temp.py
+++ src/python_alg/Temp.py
@@ -1,2 +1,10 @@
 import sys
 import heapq
+###24860
+n1, n2 = map(int, sys.stdin.readline().strip().split(" "))
+m1, m2 = map(int, sys.stdin.readline().strip().split(" "))
+o1, o2, o3 = map(int, sys.stdin.readline().strip().split(" "))
+
+result = (n1 * n2 + m1 * m2) * (o1 * o2 * o3)
+print(result)
+###
