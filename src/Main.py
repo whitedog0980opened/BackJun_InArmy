126af9c91c51481f4d68b6ca75dd720e75692eea
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-01-22 14:16:29 +0000

    n1158py stocked

--- src/Main.py
+++ src/Main.py
@@ -1,4 +1,20 @@
 import sys
 import heapq
+from collections import deque
+#from collections import deque
+n, k = map(int, sys.stdin.readline().strip().split(" "))
+k -= 1
 
+dq = deque([])
+for i in range(n):
+    dq.append(i + 1)
 
+dq.rotate(-k)
+sys.stdout.write("<" + str(dq.popleft()))
+dq.rotate(-k)
+
+for i in range(n - 1):
+    sys.stdout.write(", " + str(dq.popleft()))
+    dq.rotate(-k)
+sys.stdout.write(">")
+sys.stdout.flush()
--- src/python_alg/Temp.py
+++ src/python_alg/Temp.py
@@ -1,5 +1,6 @@
 import sys
 import heapq
+from collections import deque
 ###24860
 n1, n2 = map(int, sys.stdin.readline().strip().split(" "))
 m1, m2 = map(int, sys.stdin.readline().strip().split(" "))
@@ -8,3 +9,23 @@ o1, o2, o3 = map(int, sys.stdin.readline().strip().split(" "))
 result = (n1 * n2 + m1 * m2) * (o1 * o2 * o3)
 print(result)
 ###
+
+###1158
+#from collections import deque
+n, k = map(int, sys.stdin.readline().strip().split(" "))
+k -= 1
+
+dq = deque([])
+for i in range(n):
+    dq.append(i + 1)
+
+dq.rotate(-k)
+sys.stdout.write("<" + str(dq.popleft()))
+dq.rotate(-k)
+
+for i in range(n - 1):
+    sys.stdout.write(", " + str(dq.popleft()))
+    dq.rotate(-k)
+sys.stdout.write(">")
+sys.stdout.flush()
+###1158
