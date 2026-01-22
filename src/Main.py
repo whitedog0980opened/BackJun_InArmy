6da081f3d6ae252633d1a3c51be12cea6dd0d735
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-01-22 13:50:49 +0000

    n24860py not solved yet, just stocked

--- src/Main.py
+++ src/Main.py
@@ -1,9 +1,4 @@
 import sys
 import heapq
 
-n1, n2 = map(int, sys.stdin.readline().strip().split(" "))
-m1, m2 = map(int, sys.stdin.readline().strip().split(" "))
-o1, o2, o3 = map(int, sys.stdin.readline().strip().split(" "))
 
-result = (n1 * n2 + m1 * m2) * (o1 * o2 * o3)
-print(result)
