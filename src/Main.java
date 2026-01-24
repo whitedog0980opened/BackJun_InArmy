6ef8d58688ac64d1c36d5f2dafba5545fb8ad499
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-01-24 14:19:04 +0000

    n31925py solved

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,24 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int cases = Integer.parseInt(br.readLine());
+        ArrayList<String> ableNames = new ArrayList<>();
+
+        for (int i = 0; i < cases; i++) {
+            String[] inputs = br.readLine().split(" ");
+            int maxScore = Integer.parseInt(inputs[3]);
+            int crrScore = Integer.parseInt(inputs[4]);
+            if (inputs[1].equals("jaehak") && inputs[2].equals("notyet") && (maxScore > 3 || maxScore == -1)) {
+                ableNames.add(inputs[0]);
+            }
+        }
+
+        Collections.sort(ableNames);
+        bw.write(Integer.toString(ableNames.size()) + "\n");
+        for (String name : ableNames) {
+            bw.write(name + "\n");
+        }
+        
         
         bw.flush();
         bw.close();
--- src/python_alg/Silver.py
+++ src/python_alg/Silver.py
@@ -36,3 +36,25 @@ max_value = max(dict.values())
 sys.stdout.write(str(max_value))
 sys.stdout.flush()
 ###
+
+###31925
+cases = int(sys.stdin.readline().strip())
+ables = []
+for i in range(cases):
+    name, on_school, is_winner, max_price, crr_price = input().strip().split()
+    if (on_school == "jaehak" and
+        is_winner == "notyet" and
+        (int(max_price) > 3 or int(max_price) == -1)):
+        ables.append([int(crr_price), name])
+
+ables.sort(key=lambda x: (x[0]))
+ables = ables[:10]
+
+able = len(ables)
+sys.stdout.write(str(able) + "\n")
+ables.sort(key=lambda x: (x[1]))
+for able in ables:
+    sys.stdout.write(able[1] + "\n")
+
+sys.stdout.flush()
+###
