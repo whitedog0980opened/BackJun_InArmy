115ed1d8b6f2732348291d1522f6767d604690a0
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-01-24 14:52:21 +0000

    n1918 solving

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,16 @@ import java.util.stream.IntStream;
 //want to study : hashTable
 //need to review = 5430 ! deque
 
-
+//1918
+// split 정규식보다, Matcher 라는 클래스를 이용해보길 권장. (복잡한 수식이기때문)
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int cases = Integer.parseInt(br.readLine());
-
+    
+        String ori = br.readLine();
+        String[] nums = ori.split("[+-*/]");
         
         
         bw.flush();
