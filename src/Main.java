590a2ec5657371b07f71efcf632585298a600f6a
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-01-25 14:08:01 +0000

    n30503 solving

--- src/Main.java
+++ src/Main.java
@@ -11,19 +11,45 @@ import java.util.regex.Matcher;
 //https://lmarena.ai/ko
 
 public class Main {
+    //instant Stack
+    //30503
+    //not tested
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int strNum = Integer.parseInt(st.nextToken());
+        int mapSize = Integer.parseInt(st.nextToken());
 
-        String a =  "Aa" + "a".repeat(strNum - 2);;
-        String b =  "BB" + "a".repeat(strNum - 2);;
+        int[] map = new int[mapSize + 1]; //1 starting
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i < mapSize + 1; i++) {
+            map[i] = Integer.parseInt(st.nextToken());
+        }
 
-        // bw.write(Integer.toString(a.hashCode()));
-        bw.write(a + "\n");
-        bw.write(b + "\n");
+        int quaryNum = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < quaryNum; i++) {
+            String[] inputs = br.readLine().split(" ");
+            if (inputs.length == 3) {
+                int from = Integer.parseInt(inputs[1]);
+                int to = Integer.parseInt(inputs[2]);
+                for (int j = from; j <= to; j++) {
+                    map[j] = Integer.MAX_VALUE;
+                }
+            }
+            else {
+                int from = Integer.parseInt(inputs[1]);
+                int to = Integer.parseInt(inputs[2]);
+                int findCase = Integer.parseInt(inputs[3]);
+                
+                int counter = 0;
+                for (int j = from; j <= to; j++) {
+                    if (map[j] == findCase) counter++;
+                }
+                bw.write(Integer.toString(counter) + "\n");
+            }
+        }
 
         bw.flush();
         bw.close();
