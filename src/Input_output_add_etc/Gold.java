1d91b7ebcb6d8b96f622c8a8df3213251ff81fd8
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-01-25 07:28:40 +0000

    n1504 solved from stock

--- src/Input_output_add_etc/Gold.java
+++ src/Input_output_add_etc/Gold.java
@@ -1012,5 +1012,93 @@ public class Gold {
         }
         return disk;
     }
+    static void n1504() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int nodeNum = Integer.parseInt(st.nextToken());
+        int graphNum = Integer.parseInt(st.nextToken());
+
+        ArrayList<int[]>[] graphs = new ArrayList[nodeNum + 1];
+        //init graphs
+        for (int i = 1; i < nodeNum + 1; i++) {
+            graphs[i] = new ArrayList<int[]>();
+        }
+        //get inputs from Buffer
+        for (int i = 0; i < graphNum; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            
+            graphs[from].add(new int[]{to, weight});
+            graphs[to].add(new int[]{from, weight});
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int target1 = Integer.parseInt(st.nextToken());
+        int target2 = Integer.parseInt(st.nextToken());
+        int targetFinal = nodeNum;
+        st = null;
+
+        int toTarget1 = bfs1504(1, target1, nodeNum, graphNum, graphs);
+        int toTarget2 = bfs1504(1, target2, nodeNum, graphNum, graphs);
+        int betweenTgs = bfs1504(target1, target2, nodeNum, graphNum, graphs);
+        int toFinal1 = bfs1504(target1, targetFinal, nodeNum, graphNum, graphs);
+        int toFinal2 = bfs1504(target2, targetFinal, nodeNum, graphNum, graphs);
+
+        boolean[] wayNot = {true, true};
+        int way[] = {toTarget1 + betweenTgs + toFinal2, toTarget2 + betweenTgs + toFinal1};
+        int result = Integer.MAX_VALUE;
+        if (toTarget1 >= 0 && toFinal2 >= 0 && betweenTgs >= 0) {
+            result = Math.min(result, way[0]);
+        }
+        if (toTarget2 >= 0 && toFinal1 >= 0 && betweenTgs >= 0) {
+            result = Math.min(result, way[1]);
+        }
+
+        if (result == Integer.MAX_VALUE) {
+            result = -1;
+        }
+
+
+        bw.write(Integer.toString(result));
+        bw.flush();
+        bw.close();
+    }
+    static int bfs1504(int start, int target, int nodeNum, int graphNum, ArrayList<int[]>[] graphs) {
+        PriorityQueue<int[]> greedyQueue = new PriorityQueue<>((n1, n2) -> Integer.compare(n1[1], n2[1])); 
+        int[] startOj = new int[]{start, 0}; // index 1 : weight for priority
+        greedyQueue.add(startOj);
+
+        int disk[] = new int[nodeNum + 1];
+        Arrays.fill(disk, Integer.MAX_VALUE);
+        disk[start] = 0;
+ 
+        while (!greedyQueue.isEmpty()) {
+            int[] crr = greedyQueue.poll();
+
+            if (crr[0] == target) {
+                // shortestWay = crr[2];
+            }
+
+            for (int[] nextPoint : graphs[crr[0]]) {
+                // shoter then disk
+                if (disk[nextPoint[0]] > disk[crr[0]] + nextPoint[1]) {
+                    disk[nextPoint[0]] = disk[crr[0]] + nextPoint[1];
+                    int[] next = new int[]{nextPoint[0], crr[1] + nextPoint[1]};
+                    greedyQueue.add(next);
+                }
+            }
+        }
+
+        if (disk[target] == Integer.MAX_VALUE) {
+            return -1;
+        }
+        else {
+            return disk[target];
+        }
+    }
 
 }
