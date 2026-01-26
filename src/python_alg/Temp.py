d8134b5e3913f88d47bf25eaa41fe8fdf0633cb3
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-01-26 13:00:39 +0000

    n1158py solved from stock

--- src/python_alg/Temp.py
+++ src/python_alg/Temp.py
@@ -2,22 +2,4 @@ import sys
 import heapq
 from collections import deque
 
-###1158
-#from collections import deque
-n, k = map(int, sys.stdin.readline().strip().split(" "))
-k -= 1
 
-dq = deque([])
-for i in range(n):
-    dq.append(i + 1)
-
-dq.rotate(-k)
-sys.stdout.write("<" + str(dq.popleft()))
-dq.rotate(-k)
-
-for i in range(n - 1):
-    sys.stdout.write(", " + str(dq.popleft()))
-    dq.rotate(-k)
-sys.stdout.write(">")
-sys.stdout.flush()
-###1158
