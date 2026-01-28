d2f5f89c913c730ae639085b8947bff41ce123fd
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-01-28 14:37:22 +0000

    n1014 solving

--- src/Main.java
+++ src/Main.java
@@ -41,11 +41,15 @@ public class Main {
             }
 
             //cult first floor
-            for (int i = 0; i < )
+            int maxAbleTry = (int) Math.pow(2, m);
+            for (int j = 0; j < maxAbleTry; j++) {
+                //여기에서 j값을 2진수로써 사용
+                //아마, boolean 배열을 사용할듯
+                //연속된 값하고, X쳐진값 (이건 map[0][A]에서 찾을 수 있을듯.) 을 걸러야함
+                //j의 십진수값이 어느 인덱스가 true인지를 알아야 할듯. 이건 함수로 만들자
+                // -> 10진수값 인풋, boolean배열 리턴
 
-
-
-
+            }
 
         }
 
