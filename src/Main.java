4ab99cca99e656aceb40d00b37d5ea3957d512a4
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-01-29 14:07:45 +0000

    n1014 save

--- src/Main.java
+++ src/Main.java
@@ -40,17 +40,38 @@ public class Main {
                 }
             }
 
-            //cult first floor
-            int maxAbleTry = (int) Math.pow(2, m);
-            for (int j = 0; j < maxAbleTry; j++) {
-                //여기에서 j값을 2진수로써 사용
-                //아마, boolean 배열을 사용할듯
-                //연속된 값하고, X쳐진값 (이건 map[0][A]에서 찾을 수 있을듯.) 을 걸러야함
-                //j의 십진수값이 어느 인덱스가 true인지를 알아야 할듯. 이건 함수로 만들자
-                // -> 10진수값 인풋, boolean배열 리턴
+            //첫번째 줄은 단순히 2^m의 가짓수를 전부 시험하고 이 중 조건에 맞는 경우를 고른다
+            //자료구조를 고민중
+            //자료구조가 저장하는 내용은 2진수int(map) 과 지금까지 앉은 학생의 갯수이다.
+            //2층부터의 계산은 전 층에서의 모든 저장 내용을 반복한다.
+            // 전층의 학생 위치에 따라 설치 불가능한 위치를 갱신한다. 이는 2진법으로 비트마스크 사용
+            // 가능한 수를 전부 다음층의 자료구조에 저장한다.
+            //  중첩이 많이 일어난다. 이는 학생수가 더 많은것을 저장한다.
+            //자료구조는 int[]로 결정. 최대 길이가 1024이므로 그닥 길진않다.
 
+            int totalWays = (int) Math.pow(2, m);
+            int[][] bitDp = new int[n][totalWays]; // [현재 층][학생의 배치2진법의 10진수값] = 앉은 학생 수
+            //1floor
+            for (int j = 0; j < totalWays; j++) {
+                int crrDecimal = j;
+                boolean isAble = true; // 조건에 충족하는가
+                // 연속하는 1이 존재하는가
+                if ((crrDecimal & (crrDecimal << 1)) != 0) {
+                    continue;
+                }
+                for (int k = 0; k < m; k++) {
+                    //해당 k에서 맵에 자리가 앖는데, crr에는 1인경우
+                    if (!map[0][k] && ((crrDecimal & (1 << k)) != 0)) {
+                        isAble = false;
+                        break;
+                    }
+                }
+                
+                if (isAble) {
+                    //중복 X
+                    bitDp[0][crrDecimal] = Integer.bitCount(crrDecimal);
+                }
             }
-
         }
 
 
