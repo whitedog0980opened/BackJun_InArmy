f9b30669a8fcf55b48895faaa31806b04ab53434
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-01-29 14:55:45 +0000

    1014 comment edited

--- src/Main.java
+++ src/Main.java
@@ -14,11 +14,21 @@ public class Main {
 
     //1014
     public static void main(String[] args) throws IOException {
+        //초기 고찰
         // 비트마스크를 이용. 규칙을 정하고 모든 경우의 수를 계산한다 (가능하면 비트계산)
         // 규칙에 부합하는 비트마스크를 dp에 저장한다. 
         // 그다음, 다음줄은 기본규칙(부서진 책상) + 이전줄에 의해 컨닝방지 규칙 + 규칙을 포함한 규칙 (X))
         // + 오류 케이스. 첫줄이 전부 X일 경우 무조건 0 출력됨
         // 함수화 필요. 예외 사항에 대응하기 때문에 특정 기능 코드 재사용 필요.
+
+        //중반 고찰
+        //첫번째 줄은 단순히 2^m의 가짓수를 전부 시험하고 이 중 조건에 맞는 경우를 고른다
+        //DP자료구조가 저장하는 내용은 2진수int(map) 과 지금까지 앉은 학생의 갯수이다.
+        //2층부터의 계산은 전 층에서의 모든 저장 내용을 반복한다.
+        // 전층의 학생 위치에 따라 설치 불가능한 위치를 갱신한다. 이는 2진법으로 비트마스크 사용
+        // 가능한 수를 전부 다음층의 자료구조에 저장한다.
+        //  중첩이 많이 일어난다. 이는 학생수가 더 많은것을 저장한다.
+        //자료구조는 int[]로 결정. 최대 길이가 1024이므로 그닥 길진않다.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -30,8 +40,8 @@ public class Main {
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
 
-            boolean[][] map = new boolean[n][m];
-
+            boolean[][] map = new boolean[n][m]; //un seatAble
+            //take input
             for (int j = 0; j < n; j++) {
                 st = new StringTokenizer(br.readLine());
                 String[] input = st.nextToken().split("");
@@ -40,19 +50,10 @@ public class Main {
                 }
             }
 
-            //첫번째 줄은 단순히 2^m의 가짓수를 전부 시험하고 이 중 조건에 맞는 경우를 고른다
-            //자료구조를 고민중
-            //자료구조가 저장하는 내용은 2진수int(map) 과 지금까지 앉은 학생의 갯수이다.
-            //2층부터의 계산은 전 층에서의 모든 저장 내용을 반복한다.
-            // 전층의 학생 위치에 따라 설치 불가능한 위치를 갱신한다. 이는 2진법으로 비트마스크 사용
-            // 가능한 수를 전부 다음층의 자료구조에 저장한다.
-            //  중첩이 많이 일어난다. 이는 학생수가 더 많은것을 저장한다.
-            //자료구조는 int[]로 결정. 최대 길이가 1024이므로 그닥 길진않다.
-
             int totalWays = (int) Math.pow(2, m);
             int[][] bitDp = new int[n][totalWays]; // [현재 층][학생의 배치2진법의 10진수값] = 앉은 학생 수
             //1floor
-            int successCounter = 0; //만약 전부 X이면, 나중에 자리가 있어도 바로 스킵되므로 이를 방지하기 위한 변수임
+            int successCounter = 0; //var for try next floor if unSeatable on before floor
             boolean exceptionCatchar = false; // 위 변수 쓸일 있으면 true
 
             for (int j = 0; j < totalWays; j++) {
