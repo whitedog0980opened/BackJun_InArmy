1ae3fe2625ffa04d3062c7116297c2320879f78a
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-01-30 13:21:39 +0000

    n25246py stocked

--- src/Main.py
+++ src/Main.py
@@ -2,3 +2,11 @@ import sys
 import heapq
 from collections import deque
 
+###25246
+origin_str = sys.stdin.readline().strip()
+to_find = "aeiou"
+
+target_index = max(origin_str.rfind(c) for c in to_find)
+result_str = origin_str[:target_index + 1] + "ntry"
+print(result_str)
+###
--- src/python_alg/Temp.py
+++ src/python_alg/Temp.py
@@ -33,3 +33,12 @@ print(perimeter)
 
 sys.stdout.flush()
 ###
+
+###25246
+origin_str = sys.stdin.readline().strip()
+to_find = "aeiou"
+
+target_index = max(origin_str.rfind(c) for c in to_find)
+result_str = origin_str[:target_index + 1] + "ntry"
+print(result_str)
+###
