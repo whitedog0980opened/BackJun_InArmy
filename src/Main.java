1f1886e3fe99fcf557d502fa9e591619cc8c832d
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-02-01 07:42:49 +0000

    add solve list

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 //https://testcase.ac/problems/1764
 //https://www.acmicpc.net/step/16
-//want to solve list :
+//want to solve list : 13399 34635 28043 (marahton)
 //want to study : hashTable, 이분매칭 ,비트마스크 + dp, 그리디
 //need to review = 5430 ! deque
 //https://lmarena.ai/ko
