1e68025fea3e7ba02548247eef1e1527e94468c0
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-02-01 08:34:17 +0000

    clear main place and update to solve

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 //https://testcase.ac/problems/1764
 //https://www.acmicpc.net/step/16
-//want to solve list : 13399 34635 28043 (marahton)
+//want to solve list : 13399 28043 (marahton)
 //want to study : hashTable, 이분매칭 ,비트마스크 + dp, 그리디
 //need to review = 5430 ! deque
 //https://lmarena.ai/ko
@@ -18,26 +18,6 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int num = Integer.parseInt(br.readLine());
-        MaxHeap1655 maxH = new MaxHeap1655(num);
-        MinHeap1655 minH = new MinHeap1655(num);
-        for (int i = 0; i < num; i++) {
-            int crr = Integer.parseInt(br.readLine());
-
-            if (maxH.next == minH.next) {
-                maxH.add(crr);
-            } else {
-                minH.add(crr);
-            }
-
-            if (maxH.next > 1 && minH.next > 1 && maxH.peek() > minH.peek()) {
-                int maxVal = maxH.pop();
-                int minVal = minH.pop();
-                maxH.add(minVal);
-                minH.add(maxVal);
-            }
-        
-            bw.write(Integer.toString(maxH.peek()) + "\n");
-        }
 
         bw.flush();
         bw.close();;
