126a9e89413ee72b39b2cc8469bbc5ff16baa373
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-02-03 13:06:03 +0000

    n25246 solved

--- src/python_alg/Bronze.py
+++ src/python_alg/Bronze.py
@@ -197,3 +197,12 @@ for i in range(days):
         
     sys.stdout.write(to_print + "\n")
 ###
+
+###25246
+origin_str = sys.stdin.readline().strip()
+to_find = "aeiou"
+
+target_index = max(origin_str.rfind(c) for c in to_find)
+result_str = origin_str[:target_index + 1] + "ntry"
+print(result_str)
+###
