b50da3df9840132b4acc04b8e01c185eedd0f69d
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-02-03 14:40:14 +0000

    add n16236 failedCase

--- src/Main.java
+++ src/Main.java
@@ -11,53 +11,112 @@ import java.util.regex.Matcher;
 //https://lmarena.ai/ko
 
 public class Main {
-    //13399
+    //16236
+    //문제점 : 물고기가 도중에 성장하면, 중간부터 최단경로가 바뀐다. 
+    //기존 미리 뽑아두는경우, 바뀐내용이 반영되지 않는다
+    //+ 더 큰 물고기는 지나갈 수 없다. 이는 맨해튼 거리가 아닌 BFS사용을 유도한다.
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st;
-        int nodeNum = Integer.parseInt(br.readLine());
-        int wayNum = Integer.parseInt(br.readLine());
-        // take inputs
-        int[][] nodemap =  new int[nodeNum + 1][nodeNum + 1];
-        for (int i = 1; i < nodeNum + 1; i++) {
-            Arrays.fill(nodemap[i], 99999999);
-            nodemap[i][i] = 0;
-        }
-        //init map
-        for (int i = 0; i < wayNum; i++) {
+        int boxSize = Integer.parseInt(br.readLine());
+        int[][] box = new int[boxSize][boxSize];
+        int[] babySharkInfo = {2, 0, 0, 2}; // size, x, y, growCounter
+        for (int i = 0; i < boxSize; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            if (nodemap[from][to] > cost) nodemap[from][to] = cost;
+            for (int j = 0; j < boxSize; j++) {
+                int crr = Integer.parseInt(st.nextToken());
+                if (crr == 9) {
+                    babySharkInfo[1] = i;
+                    babySharkInfo[2] = j;
+                    crr = 0;
+                }
+                box[i][j] = crr;
+            }
         }
-        //floid
-        for (int i = 1; i < nodeNum + 1; i++) { //middle node
-            for (int j = 1; j < nodeNum + 1; j++) { //from node
-                for (int k = 1; k < nodeNum + 1; k++) {//to node
-                    int newWayCost = nodemap[j][i] + nodemap[i][k];
-                    if (nodemap[j][k] > newWayCost) nodemap[j][k] = newWayCost;
+
+        int totalTime = 0;
+        while (isBoxClear16236(boxSize, box)) {
+            ArrayList<int[]> eatables = pollEatable16236(boxSize, box, babySharkInfo);
+            if (eatables == null) { //mom calling!
+                break;
+            }
+            totalTime += sharkHuntTIme16236(boxSize, box, babySharkInfo, eatables);
+        }
+        
+        bw.write(Integer.toString(totalTime));
+        bw.flush();
+        bw.close();;
+    }
+    private static ArrayList<int[]> pollEatable16236(int boxSize, int[][] box, int[] babySharkInfo) {
+        ArrayList<int[]> eatableFishInfo = new ArrayList<>();
+        int babySharkSize = babySharkInfo[0];
+        for (int i = 0; i < boxSize; i++) {
+            for (int j = 0; j < boxSize; j++) {
+                if (box[i][j] > 0 && box[i][j] < babySharkSize) {
+                    eatableFishInfo.add(new int[]{i, j});
+                    box[i][j] = 0;
                 }
             }
         }
-        //unreachable 99999999 -> 0
-        for (int i = 1; i < nodeNum + 1; i++) {
-            for (int j = 1; j < nodeNum + 1; j++) {
-                if (nodemap[i][j] == 99999999) nodemap[i][j] = 0;
+        if (eatableFishInfo.isEmpty()) { //Mom Calling
+            return null;
+        }
+        return eatableFishInfo;
+    }
+    //poll close fish
+    private static int[] findCloseFish16236(int[] babySharkInfo, ArrayList<int[]> eatableFishInfo) {
+        int[] closeFish = {Integer.MAX_VALUE, 0, 0};
+        int closeFishIndex = -1;
+        int eatableFishNum = eatableFishInfo.size();
+        for (int i = 0; i < eatableFishNum; i++) {
+            int[] crrFish = eatableFishInfo.get(i);
+            int time = Math.abs(babySharkInfo[1] - crrFish[0]) + Math.abs(babySharkInfo[2] - crrFish[1]);
+            boolean okay = false;
+            //logic -> prioty time->x small->y small 
+            if (closeFish[0] > time) {
+                okay = true;
+            }
+            else if (closeFish[0] == time)  {
+                if (closeFish[1] > crrFish[0]) okay = true;
+                else if (closeFish[1] == crrFish[0]) {
+                    if (closeFish[2] > crrFish[1]) okay = true;
+                }
+            }
+
+            if (okay) {
+                closeFish[0] = time;
+                closeFish[1] = crrFish[0];
+                closeFish[2] = crrFish[1];
+                closeFishIndex = i;
             }
         }
-        //output
-        for (int i = 1; i < nodeNum + 1; i++) {
-            bw.write(Integer.toString(nodemap[i][1]));
-            for (int j = 2; j < nodeNum + 1; j++) {
-                bw.write(" " + Integer.toString(nodemap[i][j]));
+        eatableFishInfo.remove(closeFishIndex);
+        return closeFish;
+    }
+    private static int sharkHuntTIme16236(int boxSize, int[][] box, int[] babySharkInfo, ArrayList<int[]> eatableFishInfo) {
+        int totalTime = 0; 
+        while (!eatableFishInfo.isEmpty()) {
+            int[] closeFish = findCloseFish16236(babySharkInfo, eatableFishInfo);
+            totalTime += closeFish[0];
+            //eat enough fishes to grow
+            if (--babySharkInfo[3] == 0) {
+                babySharkInfo[0]++;
+                babySharkInfo[3] = babySharkInfo[0];
             }
-            bw.newLine();
+            babySharkInfo[1] = closeFish[1];
+            babySharkInfo[2] = closeFish[2];
         }
-        bw.flush();
-        bw.close();;
+        return totalTime;
+    }
+    private static boolean isBoxClear16236(int boxSize, int[][] box) {
+        for (int i = 0; i < boxSize; i++) {
+            for (int j = 0; j < boxSize; j++) {
+                if (box[i][j] != 0) return true;
+            }
+        }
+        return false;
     }
 }
