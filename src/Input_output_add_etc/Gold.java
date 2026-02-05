564a582d87ef3486f98e51adff51d286b02bab48
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-02-05 14:47:05 +0000

    n2206 solved

--- src/Input_output_add_etc/Gold.java
+++ src/Input_output_add_etc/Gold.java
@@ -1485,4 +1485,69 @@ public class Gold {
         box[targetFish[0]][targetFish[1]] = 0; //eaten
         return targetFish[2];
     }
+    private static void n2206() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        boolean[][] map = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String inputLine = br.readLine();
+            for (int j = 0; j < m; j++) {
+                char ch = inputLine.charAt(j);
+                if (ch == '1') map[i][j] = true;
+            }
+        }
+
+        int[] startPoint = {0, 0, 1, 1}; //n, m, distance, breakable count
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(startPoint);
+        boolean[][] visited = new boolean[n][m];
+        boolean[][] bVisited = new boolean[n][m];
+        visited[0][0] = true;
+
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+
+        int result = 0;
+        while (!queue.isEmpty()) {
+            int[] crrPoint = queue.poll();
+
+            if (crrPoint[0] == n - 1 && crrPoint[1] == m - 1) { //found
+                result = crrPoint[2];
+                break;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nextN = crrPoint[0] + dx[i];
+                int nextM = crrPoint[1] + dy[i];
+                int[] nextPoint = {nextN, nextM, crrPoint[2] + 1, crrPoint[3]};
+                if (nextN < 0 || nextN >= n || nextM < 0 || nextM >= m) continue;
+                if (crrPoint[3] == 1) {
+                    if (visited[nextN][nextM]) continue;
+                    if (map[nextN][nextM]) {
+                        nextPoint[3] = 0;
+                    } else {
+                        visited[nextN][nextM] = true;
+                    }
+                    bVisited[nextN][nextM] = true;
+                } else {
+                    if (bVisited[nextN][nextM]) continue;
+                    if (map[nextN][nextM]) continue;
+                    bVisited[nextN][nextM] = true;
+                }
+
+                queue.add(nextPoint);
+            }
+        }
+        if (result == 0) result = -1;
+        bw.write(Integer.toString(result));
+
+        bw.flush();
+        bw.close();;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -11,23 +11,6 @@ import java.util.regex.Matcher;
 //https://lmarena.ai/ko
 
 public class Main {
-    // 이런 전용 클래스를 하나 만들면 contains(new IntArrayWrapper(arr))로 안전하게 확인 가능
-    private static class IntArrayWrapper {
-        private final int[] data;
-        public IntArrayWrapper(int[] data) { this.data = data; }
-
-        @Override
-        public boolean equals(Object o) {
-            if (!(o instanceof IntArrayWrapper)) return false;
-            return Arrays.equals(this.data, ((IntArrayWrapper) o).data);
-        }
-
-        @Override
-        public int hashCode() {
-            return Arrays.hashCode(data);
-        }
-    }
-    //디버깅 필요 15650 -> 예제 2. 출력 이상함
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -35,46 +18,61 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[] arr = new int[m];
-        for (int i = 0; i < m; i++) {
-            arr[i] = i + 1;
-        }
-        HashSet<IntArrayWrapper> hs = new HashSet<>();
         
-        fLoop : while (true) {
-            int crrIndex = m - 1;
-            boolean wrongCase = true;
-            while (wrongCase) {
-                wrongCase = false;
-                for (int i = 0; i < m; i++) {
-                    if (arr[i] + (m-1 - i) > n) { //이상한거 찾음
-                        // wrongCase = true;
-                        if (i - 1 < 0) break fLoop;
-                        for (int j = (i - 1); j < m; j++) { //정상화
-                            arr[j]++;
-                            if (j + 1 > m - 1) break;
-                            arr[j + 1] = arr[j];
-                        }
-                        if (arr[i - 1] + (m - 1 - i + 1) > n) wrongCase = true;
-                        if (wrongCase && i - 1 == 0) break fLoop;
-                    }
-                }
-            }
+        boolean[][] map = new boolean[n][m];
 
-            crrIndex = m - 1;
-            //중복 체크
-            IntArrayWrapper tempWapped = new IntArrayWrapper(Arrays.copyOf(arr, m));
-            if (hs.contains(tempWapped)) continue;
-            hs.add(tempWapped);
-            //print
-            bw.write(Integer.toString(arr[0]));
-            for (int i = 1; i < m; i++) {
-                bw.write(" " + Integer.toString(arr[i]));
+        for (int i = 0; i < n; i++) {
+            String inputLine = br.readLine();
+            for (int j = 0; j < m; j++) {
+                char ch = inputLine.charAt(j);
+                if (ch == '1') map[i][j] = true;
             }
-            bw.newLine();
-
-            arr[crrIndex]++;
         }
+
+        int[] startPoint = {0, 0, 1, 1}; //n, m, distance, breakable count
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(startPoint);
+        boolean[][] visited = new boolean[n][m];
+        boolean[][] bVisited = new boolean[n][m];
+        visited[0][0] = true;
+
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+
+        int result = 0;
+        while (!queue.isEmpty()) {
+            int[] crrPoint = queue.poll();
+
+            if (crrPoint[0] == n - 1 && crrPoint[1] == m - 1) { //found
+                result = crrPoint[2];
+                break;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nextN = crrPoint[0] + dx[i];
+                int nextM = crrPoint[1] + dy[i];
+                int[] nextPoint = {nextN, nextM, crrPoint[2] + 1, crrPoint[3]};
+                if (nextN < 0 || nextN >= n || nextM < 0 || nextM >= m) continue;
+                if (crrPoint[3] == 1) {
+                    if (visited[nextN][nextM]) continue;
+                    if (map[nextN][nextM]) {
+                        nextPoint[3] = 0;
+                    } else {
+                        visited[nextN][nextM] = true;
+                    }
+                    bVisited[nextN][nextM] = true;
+                } else {
+                    if (bVisited[nextN][nextM]) continue;
+                    if (map[nextN][nextM]) continue;
+                    bVisited[nextN][nextM] = true;
+                }
+
+                queue.add(nextPoint);
+            }
+        }
+        if (result == 0) result = -1;
+        bw.write(Integer.toString(result));
+
         bw.flush();
         bw.close();;
     }
