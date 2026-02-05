c55f94f71ceba91a3b2a0cc934335941ef48e332
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-02-05 13:05:26 +0000

    save

--- src/Main.py
+++ src/Main.py
@@ -3,25 +3,12 @@ import heapq
 from collections import deque
 
 ###15786
-regex_len, posts = map(int, sys.stdin.readline().strip().split())
-regex = sys.stdin.readline().strip()
+# regex_len, posts = map(int, sys.stdin.readline().strip().split())
+len_arr = int(sys.stdin.readline().strip())
+arr_def = map(bool, sys.stdin.readline().strip().split())
+arr_tar = map(bool, sys.stdin.readline().strip().split())
 
-for i in range(posts):
-    is_able = True;
-    postit = sys.stdin.readline().strip()
-    sliced_postit = postit
-    for ch in regex:
-        if (ch in sliced_postit):
-            slice_point = sliced_postit.index(ch) + 1
-            sliced_postit = sliced_postit[slice_point:]
-        else:
-            is_able = False;
-            break;
-        
-    if (is_able):
-        sys.stdout.write("true\n")
-    else:
-        sys.stdout.write("false\n")
+f
     
 sys.stdout.flush();
 ###
