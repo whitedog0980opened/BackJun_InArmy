e9093fcc7e3efbf41259e2bca0785d1fc8ce9254
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-02-07 13:24:14 +0000

    n31534py solved

--- src/python_alg/Silver.py
+++ src/python_alg/Silver.py
@@ -147,3 +147,20 @@ print(result)
 
 sys.stdout.flush();
 ###
+
+###31534
+a, b, h = map(float, sys.stdin.readline().strip().split())
+if (a == b):
+    result = -1; 
+else:
+    h = math.sqrt(abs(b - a) ** 2 + h ** 2)
+
+    l = h / abs(a - b)
+    al = a * l
+    bl = b * l
+    ar = (al ** 2) * math.pi
+    br = (bl ** 2) * math.pi
+    result = round(abs(ar - br), 6)
+print(result)
+sys.stdout.flush();
+###
