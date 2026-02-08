87b0a4c0f560aab1dcbe6ff06887643820e463aa
whitedog0980opened1 <whitedog0980opened1@gmail.com>
2026-02-08 14:19:48 +0000

    n1916 solved

--- src/Input_output_add_etc/Gold.java
+++ src/Input_output_add_etc/Gold.java
@@ -1702,4 +1702,60 @@ public class Gold {
         bw.flush();
         bw.close();
     }
+    private static void n1916() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int nodeNum = Integer.parseInt(br.readLine());
+        int busNum = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int[][] map = new int[nodeNum + 1][nodeNum + 1];
+        for (int i = 1; i < nodeNum + 1; i++) {
+            Arrays.fill(map[i], Integer.MAX_VALUE);
+            // map[i][i] = 0;
+        }
+
+        for (int i = 0; i < busNum; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            if (map[from][to] > cost) map[from][to] = cost;
+        }
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a1, a2) -> {
+            return a1[1] - a2[1];
+        });
+        int[] disk = new int[nodeNum + 1];
+        st = new StringTokenizer(br.readLine());
+        int startNode = Integer.parseInt(st.nextToken());
+        int endNode = Integer.parseInt(st.nextToken());
+        int[] startArr = {startNode, 0}; //cost
+
+        pq.add(startArr);
+        disk[startNode] = 0;
+        Arrays.fill(disk, Integer.MAX_VALUE);
+        int lessCost = 0;
+        while (!pq.isEmpty()) {
+            int[] crrArr = pq.poll();
+
+            if (crrArr[0] == endNode) {
+                lessCost = crrArr[1];
+                break;
+            }
+            for (int i = 1; i < nodeNum + 1; i++) {
+                int nextCost = map[crrArr[0]][i];
+                if (nextCost == Integer.MAX_VALUE) continue;
+                nextCost += crrArr[1];
+                if (disk[i] > nextCost) {
+                    disk[i] = nextCost;
+                    int[] nextNode = {i, nextCost};
+                    pq.add(nextNode);
+                }
+            }
+        }
+        bw.write(Integer.toString(lessCost));
+        bw.flush();
+        bw.close();
+    }
 }
