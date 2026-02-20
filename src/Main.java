eda4829d3d5ddfae562199177a1ff02b511ee841
whitedog0980opened <whitedog0980opened@gmail.com>
2026-02-20 14:51:32 +0000

    n1004 solving

--- src/Main.java
+++ src/Main.java
@@ -14,66 +14,41 @@ import java.util.regex.Matcher;
 //https://lmarena.ai/ko
 
 public class Main {
-    private static class Node1987 {
-        int x;
-        int y;
-        int depth;
-        boolean[] visitedAlph;
-
-        Node1987(int x, int y, int depth, boolean[] visitedAlph) {
-            this.x = x;
-            this.y = y;
-            this.depth = depth;
-            this.visitedAlph = visitedAlph;
-        }
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int r = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int startX = Integer.parseInt(st.nextToken());
+            int startY = Integer.parseInt(st.nextToken());
+            int endX = Integer.parseInt(st.nextToken());
+            int endY = Integer.parseInt(st.nextToken());
 
-        int[][] node = new int[r][c];
+            int planetNum = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < r; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < c; j++) {
-                node[i][j] = ((int) line.charAt(j)) - 65;
+            int needToPene = 0;
+            for (int i = 0; i < planetNum; i++) {
+                st = new StringTokenizer(br.readLine());
+                int planetX = Integer.parseInt(st.nextToken());
+                int planetY = Integer.parseInt(st.nextToken());
+                int planetR = Integer.parseInt(st.nextToken());
+
+                double startToPlX = Math.abs(planetX - startX);
+                double startToPlY = Math.abs(planetY - startY);
+                double endToPlX = Math.abs(planetX - endX);
+                double endToPlY = Math.abs(planetX - endY);
+
+
+                boolean startInnerPlanet = Math.sqrt(Math.pow(startToPlX, 2) + Math.pow(startToPlY, 2)) < planetR;
+                boolean endInnerPlanet = Math.sqrt(Math.pow(endToPlX, 2) + Math.pow(endToPlY, 2)) < planetR;
+                if (startInnerPlanet != endInnerPlanet) needToPene++;
             }
+
+            bw.write(Integer.toString(needToPene) + "\n");
         }
 
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
-
-        boolean[] startChecker = new boolean[26];
-        startChecker[node[0][0]] = true;
-        Node1987 startNode = new Node1987(0, 0, 1, startChecker);
-        Stack<Node1987> stack = new Stack<>();
-        stack.add(startNode);
-
-        int maxDepth = 0;
-        while (!stack.isEmpty()) {
-            Node1987 crrNode = stack.pop();
-            maxDepth = Math.max(maxDepth, crrNode.depth);
-            
-            for (int i = 0; i < 4; i++) {
-                int nextX = crrNode.x + dx[i];
-                int nextY = crrNode.y + dy[i];
-                if (nextX < 0 || nextX >= r || nextY < 0 || nextY >= c) continue; // border check
-                int nextAlph = node[nextX][nextY];
-                if (crrNode.visitedAlph[nextAlph]) continue; //already exist
-
-                boolean[] nextVisited = crrNode.visitedAlph.clone();
-                nextVisited[nextAlph] = true;
-                int nextDepth = crrNode.depth + 1;
-                Node1987 nextNode = new Node1987(nextX, nextY, nextDepth, nextVisited);
-                stack.add(nextNode);
-            }
-        }
-        
-        bw.write(Integer.toString(maxDepth));
         bw.flush();
         bw.close();
     }
