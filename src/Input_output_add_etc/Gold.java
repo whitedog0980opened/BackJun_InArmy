99458fd66d58ddc76ec657d0c612eda257b2bcd4
whitedog0980opened <whitedog0980opened@gmail.com>
2026-03-15 10:33:58 +0000

    n2638 solved

--- src/Input_output_add_etc/Gold.java
+++ src/Input_output_add_etc/Gold.java
@@ -1966,4 +1966,96 @@ public class Gold {
         };
         return result;
     }
+    private static void n2638() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        boolean[][] cheeseMap = new boolean[n][m];
+        boolean[][] isAir = new boolean[n][m];
+        int[][] cheeseExCounter = new int[n][m];
+
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+        //cheeseMap init
+        //모서리에는 치즈가 존재하지 않으므로, 제외
+        boolean isNothing = true;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m - 1; j++) {
+                boolean crrCheese = Integer.parseInt(st.nextToken()) == 0 ? false : true;
+                cheeseMap[i][j] = crrCheese;
+                if (crrCheese) isNothing = false;
+            }
+        }
+        markExternalAir2638(n, m, cheeseMap, isAir);
+        //cheeseExCounter init
+        for (int i = 1; i < n - 1; i++) {
+            for (int j = 1; j < m - 1; j++) {
+                if (!cheeseMap[i][j]) continue;
+                boolean[] neighbors = new boolean[4];
+                for (int k = 0; k < 4; k++) {
+                    neighbors[k] = isAir[i + dx[k]][j + dy[k]];
+                    if (neighbors[k]) cheeseExCounter[i][j]++;
+                }
+            }
+        }
+        int time = 0;
+        while (true) {
+            isAir = new boolean[n][m];
+            markExternalAir2638(n, m, cheeseMap, isAir);
+
+            List<int[]> toMelt = new ArrayList<>();
+            boolean hasCheese = false;
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (cheeseMap[i][j]) {
+                        hasCheese = true;
+                        int airCount = 0;
+                        for (int k = 0; k < 4; k++) {
+                            int nx = i + dx[k], ny = j + dy[k];
+                            if (isAir[nx][ny]) airCount++;
+                        }
+                        if (airCount >= 2) toMelt.add(new int[]{i, j});
+                    }
+                }
+            }
+
+            if (!hasCheese) break; 
+            if (toMelt.isEmpty()) break; 
+
+            for (int[] pos : toMelt) {
+                cheeseMap[pos[0]][pos[1]] = false;
+            }
+            time++;
+        }
+        bw.write(Integer.toString(time));
+        bw.flush();
+        bw.close();
+    }
+    
+    static void markExternalAir2638(int n, int m, boolean[][] cheeseMap, boolean[][] isAir) {
+        Queue<int[]> q = new LinkedList<>();
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+        q.add(new int[]{0, 0});
+        isAir[0][0] = true;
+
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = curr[0] + dx[i];
+                int ny = curr[1] + dy[i];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+                    if (!isAir[nx][ny] && !cheeseMap[nx][ny]) {
+                        isAir[nx][ny] = true;
+                        q.add(new int[]{nx, ny});
+                    }
+                }
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -15,39 +15,92 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        long n = Long.parseLong(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        boolean[][] cheeseMap = new boolean[n][m];
+        boolean[][] isAir = new boolean[n][m];
+        int[][] cheeseExCounter = new int[n][m];
 
-        if (n == 0) {
-            bw.write("0");
-            bw.flush();
-            bw.close();
-            System.exit(0);
-        }
-        long[] matrix = {1, 1, 1, 0};
-        long[][] dp = new long[65][4];
-        dp[0] = new long[]{1, 1, 1, 0};
-
-        long[] res = {1, 0, 0, 1};
-        while (n > 0) {
-            if ((n & 1) == 1) {
-                res = matrixMult(res, matrix);
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+        //cheeseMap init
+        //모서리에는 치즈가 존재하지 않으므로, 제외
+        boolean isNothing = true;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m - 1; j++) {
+                boolean crrCheese = Integer.parseInt(st.nextToken()) == 0 ? false : true;
+                cheeseMap[i][j] = crrCheese;
+                if (crrCheese) isNothing = false;
             }
-            matrix = matrixMult(matrix, matrix);
-            n >>= 1;
         }
-        
-        bw.write(Long.toString(res[2]));
+        markExternalAir2638(n, m, cheeseMap, isAir);
+        //cheeseExCounter init
+        for (int i = 1; i < n - 1; i++) {
+            for (int j = 1; j < m - 1; j++) {
+                if (!cheeseMap[i][j]) continue;
+                boolean[] neighbors = new boolean[4];
+                for (int k = 0; k < 4; k++) {
+                    neighbors[k] = isAir[i + dx[k]][j + dy[k]];
+                    if (neighbors[k]) cheeseExCounter[i][j]++;
+                }
+            }
+        }
+        int time = 0;
+        while (true) {
+            isAir = new boolean[n][m];
+            markExternalAir2638(n, m, cheeseMap, isAir);
+
+            List<int[]> toMelt = new ArrayList<>();
+            boolean hasCheese = false;
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (cheeseMap[i][j]) {
+                        hasCheese = true;
+                        int airCount = 0;
+                        for (int k = 0; k < 4; k++) {
+                            int nx = i + dx[k], ny = j + dy[k];
+                            if (isAir[nx][ny]) airCount++;
+                        }
+                        if (airCount >= 2) toMelt.add(new int[]{i, j});
+                    }
+                }
+            }
+
+            if (!hasCheese) break; 
+            if (toMelt.isEmpty()) break; 
+
+            for (int[] pos : toMelt) {
+                cheeseMap[pos[0]][pos[1]] = false;
+            }
+            time++;
+        }
+        bw.write(Integer.toString(time));
         bw.flush();
         bw.close();
     }
-    private static long[] matrixMult(long[] mat1, long[] mat2) {
-        long divideAgent = 1000000007;
-        long[] result = {
-            ((mat1[0] * mat2[0]) % divideAgent + (mat1[1] * mat2[2]) % divideAgent) % divideAgent,
-            ((mat1[0] * mat2[1]) % divideAgent + (mat1[1] * mat2[3]) % divideAgent) % divideAgent,
-            ((mat1[2] * mat2[0]) % divideAgent + (mat1[3] * mat2[2]) % divideAgent) % divideAgent,
-            ((mat1[2] * mat2[1]) % divideAgent + (mat1[3] * mat2[3]) % divideAgent) % divideAgent
-        };
-        return result;
+    static void markExternalAir2638(int n, int m, boolean[][] cheeseMap, boolean[][] isAir) {
+        Queue<int[]> q = new LinkedList<>();
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+        q.add(new int[]{0, 0});
+        isAir[0][0] = true;
+
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = curr[0] + dx[i];
+                int ny = curr[1] + dy[i];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+                    if (!isAir[nx][ny] && !cheeseMap[nx][ny]) {
+                        isAir[nx][ny] = true;
+                        q.add(new int[]{nx, ny});
+                    }
+                }
+            }
+        }
     }
 }
